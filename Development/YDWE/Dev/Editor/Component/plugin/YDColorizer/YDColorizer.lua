10ef7c6bb26963451d5b0911134248b98925e01d
actboy168 <actboy168@gmail.com>
2013-07-30 18:41:00 +0800

    关闭颜色插件时，不再加载clr库

--- Development/YDWE/Dev/Editor/Component/plugin/YDColorizer/YDColorizer.lua
+++ Development/YDWE/Dev/Editor/Component/plugin/YDColorizer/YDColorizer.lua
@@ -1,41 +1,3 @@
---[[
-require "dotnet"
-
-local loader = {}
-	
-loader.load = function(path)
-	if global_config:get_integer("ThirdPartyPlugin.EnableDotNetSupport", 1) ~= 1 then
-		return false, 'disable'
-	end
-	if not dotnet.initialize() then
-		return false, 'not support .net.'
-	end
-
-	loader.dll = dotnet.load(path)
-	if not loader.dll then
-		return false
-	end
-	
-	local WEInit = loader.dll:GetType("WEInit")
-	if not WEInit then
-		return false, 'cannot found WEInit.'
-	end
-	
-	local s, r = dotnet.call_func(WEInit)
-			
-	if not s then
-		return false, r
-	end
-	return true
-end
-
-loader.unload = function()	
-end
-
-return loader
---]]
-require "dotnet"
-
 local loader = {}
 	
 loader.load = function(path)
@@ -43,6 +5,7 @@ loader.load = function(path)
 		return false, 'disable'
 	end
 	
+	require "dotnet"
 	local obj = dotnet:load(path, 'WEInit')
 	if not obj then
 		return false, 'load failed'
