6e1390ee8d3f86d91e3119fa4083626cd5cf1e3f
actboy168 <actboy168@gmail.com>
2013-08-06 00:30:01 +0800

    完善ansi_codecvt_facet未实现的接口

--- Development/YDWE/Dev/Editor/Core/YDWEBase/ydwe/util/detail/ansi_codecvt_facet.cpp
+++ Development/YDWE/Dev/Editor/Core/YDWEBase/ydwe/util/detail/ansi_codecvt_facet.cpp
@@ -2,13 +2,13 @@
 #include <cwchar>
 #include <windows.h>
 
-_BASE_BEGIN 
+_BASE_BEGIN
 namespace util { namespace detail {
 
 std::codecvt_base::result ansi_codecvt_facet::do_in(std::mbstate_t &, const char* from, const char* from_end, const char*& from_next, wchar_t* to, wchar_t* to_end, wchar_t*& to_next) const
 {
 	int count;
-	if ((count = ::MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, from, from_end - from, to, to_end - to)) == 0) 
+	if ((count = ::MultiByteToWideChar(CP_ACP, 0, from, from_end - from, to, to_end - to)) == 0) 
 	{
 		return error;
 	}
@@ -22,7 +22,7 @@ std::codecvt_base::result ansi_codecvt_facet::do_in(std::mbstate_t &, const char
 std::codecvt_base::result ansi_codecvt_facet::do_out(std::mbstate_t &, const wchar_t* from, const wchar_t* from_end, const wchar_t*  & from_next, char* to, char* to_end, char* & to_next) const
 {
 	int count;
-	if ((count = ::WideCharToMultiByte(CP_ACP, WC_NO_BEST_FIT_CHARS, from, from_end - from, to, to_end - to, 0, 0)) == 0)
+	if ((count = ::WideCharToMultiByte(CP_ACP, 0, from, from_end - from, to, to_end - to, 0, 0)) == 0)
 	{
 		return error;
 	}
@@ -31,6 +31,28 @@ std::codecvt_base::result ansi_codecvt_facet::do_out(std::mbstate_t &, const wch
 	to_next = to + count;
 	*to_next = '\0';
 	return ok;
+}
+
+
+std::codecvt_base::result ansi_codecvt_facet::do_unshift(std::mbstate_t&, char* to, char* /*to_end*/, char* & next) const  
+{ 
+	next = to;
+	return ok;
+} 
+
+int ansi_codecvt_facet::do_length(std::mbstate_t&, const char* from, const char* from_end, std::size_t max_limit) const
+{ 
+	int count = ::MultiByteToWideChar(CP_ACP, 0, from, from_end - from, NULL, 0);
+	if (count <= 0)
+	{
+		return 0;
+	}
+	return (static_cast<std::size_t>(count) > max_limit)? static_cast<int>(max_limit): count; 
+}
+
+int ansi_codecvt_facet::do_max_length() const throw ()
+{
+	return 2;
 }
 
 }}
--- Development/YDWE/Dev/Editor/Core/YDWEBase/ydwe/util/detail/ansi_codecvt_facet.h
+++ Development/YDWE/Dev/Editor/Core/YDWEBase/ydwe/util/detail/ansi_codecvt_facet.h
@@ -3,7 +3,7 @@
 #include <ydwe/config.h>
 #include <locale>  
 
-_BASE_BEGIN 
+_BASE_BEGIN
 namespace util { namespace detail {
 
 class ansi_codecvt_facet : public std::codecvt<wchar_t, char, std::mbstate_t>  
@@ -18,9 +18,9 @@ protected:
 	virtual int do_encoding() const throw() { return 0; }
 	virtual std::codecvt_base::result do_in(std::mbstate_t& state, const char* from, const char* from_end, const char*& from_next, wchar_t* to, wchar_t* to_end, wchar_t*& to_next) const;
 	virtual std::codecvt_base::result do_out(std::mbstate_t & state, const wchar_t* from, const wchar_t* from_end, const wchar_t*& from_next, char* to, char* to_end, char*& to_next) const;
-	virtual std::codecvt_base::result do_unshift(std::mbstate_t&, char* /*from*/, char* /*to*/, char* & /*next*/) const  { return ok; } 
-	virtual int do_length(std::mbstate_t&, const char* /*from*/, const char* /*from_end*/, std::size_t /*max*/) const  { return 0; }
-	virtual int do_max_length() const throw () { return 0; }
+	virtual std::codecvt_base::result do_unshift(std::mbstate_t&, char* to, char* to_end, char* & next) const;
+	virtual int do_length(std::mbstate_t&, const char* from, const char* from_end, std::size_t max_limit) const;
+	virtual int do_max_length() const throw ();
 };
 
 }}
