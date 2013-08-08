8523c1e803b1211349018bb4e2d6182d5e780d00
actboy168 <actboy168@gmail.com>
2013-08-08 17:44:19 +0800

    增加horrible_cast

---
+++ Development/YDWE/Dev/Editor/Core/YDWEBase/ydwe/util/horrible_cast.h
@@ -0,0 +1,17 @@
+#pragma once
+
+template <class OutputClass, class InputClass>
+union horrible_union 
+{
+	OutputClass out;
+	InputClass in;
+};
+
+template <class OutputClass, class InputClass>
+inline OutputClass horrible_cast(const InputClass input)
+{
+	horrible_union<OutputClass, InputClass> u;
+	static_assert((sizeof(InputClass)==sizeof(u)) && (sizeof(InputClass)==sizeof(OutputClass)), "OutputClass and InputClass are not the same size.");
+	u.in = input;
+	return u.out;
+}
