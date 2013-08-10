4138dc755c95b0ff2eb311ad0708738c7233476f
actboy168 <actboy168@gmail.com>
2013-08-11 00:26:37 +0800

    lua引擎支持一次启动多局游戏

--- Development/YDWE/Dev/Editor/Core/YDWEBase/ydwe/warcraft3/jass/trampoline_function.cpp
+++ Development/YDWE/Dev/Editor/Core/YDWEBase/ydwe/warcraft3/jass/trampoline_function.cpp
@@ -1,7 +1,8 @@
 #include <ydwe/warcraft3/jass/trampoline_function.h>
+#include <ydwe/warcraft3/detail/nf_register.h>
+#include <ydwe/warcraft3/hashtable.h>
 #include <ydwe/warcraft3/jass/trampoline.h>
 #include <ydwe/warcraft3/native_function.h>
-#include <ydwe/warcraft3/hashtable.h>
 #include <ydwe/util/singleton.h>
 #include <aero/function/fp_call.hpp>
 #include <map>
@@ -22,33 +23,43 @@ namespace warcraft3 { namespace jass {
 			return aero::c_call<uint32_t>(RealSetUnitAbilityLevel, unit_handle, abilcode, level);
 		}
 
-		struct trampoline_function : public std::map<uint64_t, trampoline>
+		std::map<uint64_t, trampoline> trampoline_mapping;
+		uint32_t                       jump_function_id = 0;
+		bool                           initialized = false;
+
+		void trampoline_initialize()
 		{
-			trampoline_function()
-				: JUMP_FUNC_ID(get_string_hashtable()->get("SetUnitAbilityLevel")->index_)
+			if (initialized)
 			{
-				native_function::table_hook("SetUnitAbilityLevel", (uintptr_t*)&detail::RealSetUnitAbilityLevel, (uintptr_t)detail::FakeSetUnitAbilityLevel);
+				return ;
 			}
 
-			uint32_t JUMP_FUNC_ID;
-		};
-
-		typedef util::singleton_nonthreadsafe<trampoline_function> trampoline_function_s;
+			initialized = true;
+			jump_function_id = get_string_hashtable()->get("SetUnitAbilityLevel")->index_;
+			native_function::table_hook("SetUnitAbilityLevel", (uintptr_t*)&detail::RealSetUnitAbilityLevel, (uintptr_t)detail::FakeSetUnitAbilityLevel);
+			native_function::nf_register::initialize();
+			native_function::nf_register::event_hook.connect([&]()
+			{
+				initialized = false;
+				trampoline_mapping.clear();
+				jump_function_id = 0;
+			});
+		}
 	}
 
 	uintptr_t trampoline_create(uintptr_t param_,  uintptr_t callback_)
 	{
+		detail::trampoline_initialize();
 		const static uint8_t TEMP_REGISTER = 0xD0;
-		detail::trampoline_function& tf = detail::trampoline_function_s::instance();
 
 		uint64_t key = ((uint64_t)param_ << 32) | callback_;
-		auto it = tf.find(key);
-		if (it != tf.end())
+		auto it = detail::trampoline_mapping.find(key);
+		if (it != detail::trampoline_mapping.end())
 		{
 			return it->second.code();
 		}
 
-		trampoline& op = tf[key];
+		trampoline& op = detail::trampoline_mapping[key];
 
 		op.mov(TEMP_REGISTER, jass::OPCODE_VARIABLE_INTEGER, 'YDWE');
 		op.push(TEMP_REGISTER);
@@ -56,11 +67,16 @@ namespace warcraft3 { namespace jass {
 		op.push(TEMP_REGISTER);
 		op.mov(TEMP_REGISTER, jass::OPCODE_VARIABLE_INTEGER, callback_);
 		op.push(TEMP_REGISTER);
-		op.call_native(tf.JUMP_FUNC_ID);
+		op.call_native(detail::jump_function_id);
 		op.set_ret(jass::RETURN_REGISTER);
 		op.ret();
 
 		return op.code();
+	}
+
+	void trampoline_clear()
+	{
+
 	}
 }}
 
