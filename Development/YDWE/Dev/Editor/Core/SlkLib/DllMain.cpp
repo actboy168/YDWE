228f75384d2da652c87126a67483c3eb12c4572c
actboy168 <actboy168@gmail.com>
2013-08-11 21:30:48 +0800

    去掉一个警告

--- Development/YDWE/Dev/Editor/Core/SlkLib/DllMain.cpp
+++ Development/YDWE/Dev/Editor/Core/SlkLib/DllMain.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID pReserved)
+BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID /*pReserved*/)
 {
 	if (reason == DLL_PROCESS_ATTACH)
 	{
