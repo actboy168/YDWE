469d45398f830134f71a33a4c11da03626ba3cfb
actboy168 <actboy168@gmail.com>
2013-09-10 15:56:46 +0800

    增加__newindex

--- Development/Editor/Plugin/Warcraft3/yd_lua_engine/main/libs_slk.cpp
+++ Development/Editor/Plugin/Warcraft3/yd_lua_engine/main/libs_slk.cpp
@@ -51,6 +51,11 @@ namespace warcraft3 { namespace lua_engine {
 
 	std::unique_ptr<slk_manager> slk_manager::s_ptr_;
 
+	int jass_slk_table_newindex(lua::state* /*ls*/)
+	{
+		return 0;
+	}
+
 	int jass_slk_create_proxy_table(lua::state* ls, lua::state::cfunction f, uintptr_t upvalue)
 	{
 		ls->newtable();
@@ -61,13 +66,17 @@ namespace warcraft3 { namespace lua_engine {
 				ls->pushunsigned(upvalue);
 				ls->pushcclosure(f, 1);
 				ls->rawset(-3);
+
+				ls->pushstring("__newindex");
+				ls->pushcclosure(jass_slk_table_newindex, 0);
+				ls->rawset(-3);
 			}
 			ls->setmetatable(-2);
 		}
 		return 1;
 	}
 
-	int jass_slk_object(lua::state* ls)
+	int jass_slk_object_index(lua::state* ls)
 	{
 		slk::SlkSingle* object_ptr = (slk::SlkSingle*)(uintptr_t)ls->tounsigned(lua_upvalueindex(1));
 		const char* key = ls->tostring(2);
@@ -82,7 +91,7 @@ namespace warcraft3 { namespace lua_engine {
 		return 1;
 	}
 
-	int jass_slk_table(lua::state* ls)
+	int jass_slk_table_index(lua::state* ls)
 	{
 		slk::ROBJECT_TYPE::ENUM type = (slk::ROBJECT_TYPE::ENUM)ls->tounsigned(lua_upvalueindex(1));
 		slk::object_id id;
@@ -108,13 +117,13 @@ namespace warcraft3 { namespace lua_engine {
 			return 1;
 		}
 		
-		return jass_slk_create_proxy_table(ls, jass_slk_object, (uintptr_t)&(it->second));
+		return jass_slk_create_proxy_table(ls, jass_slk_object_index, (uintptr_t)&(it->second));
 	}
 
 	int slk_create_table(lua::state* ls, const char* name, slk::ROBJECT_TYPE::ENUM type)
 	{
 		ls->pushstring(name);
-		jass_slk_create_proxy_table(ls, jass_slk_table, type);
+		jass_slk_create_proxy_table(ls, jass_slk_table_index, type);
 		ls->rawset(-3);
 		return 0;
 	}
