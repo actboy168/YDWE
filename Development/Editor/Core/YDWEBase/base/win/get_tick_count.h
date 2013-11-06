d800777581f56376158052e36e94f2535e92504b
actboy168 <actboy168@gmail.com>
2013-11-06 15:30:27 +0800

    Revert "不再使用GetTickCount"
    This reverts commit aaefa5a97ce17653c30b4a374c4f6a45b2d03111.

--- Development/Editor/Core/YDWEBase/base/win/get_tick_count.h
+++ Development/Editor/Core/YDWEBase/base/win/get_tick_count.h
@@ -1,8 +1,22 @@
 #pragma once
 
-#include <base/config.h>
 #include <cstdint>
 
 namespace base { namespace win {
-	_BASE_API uint64_t get_tick_count();
+
+	inline uint64_t get_tick_count()
+	{
+		static uint64_t s_rollover_time = 0;
+		static uint32_t s_lastnow_time  = 0;
+		uint32_t now = ::GetTickCount();
+
+		if (now < s_lastnow_time) 
+		{
+			s_rollover_time += 0x100000000I64;  // ~49.7 days.
+		}
+		s_rollover_time = now;
+
+		return now + s_rollover_time;
+	}
+
 }}
