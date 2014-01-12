4d67495a392ec8f8e1f7b1de039ca3797f2b0fee
actboy168 <actboy168@gmail.com>
2014-01-12 22:18:34 +0800

    修正luabind uint32_t转换错误的问题

--- Development/Editor/Core/YDWEBase/base/lua/luabind.h
+++ Development/Editor/Core/YDWEBase/base/lua/luabind.h
@@ -3,3 +3,4 @@
 #include <base/lua/luabind/converter_pvoid.h>
 #include <base/lua/luabind/converter_std_wstring.h>
 #include <base/lua/luabind/converter_boost_string_ref.h>
+#include <base/lua/luabind/converter_uint32_t.h>
