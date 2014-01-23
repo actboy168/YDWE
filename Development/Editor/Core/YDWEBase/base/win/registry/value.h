6dcf21af215ca254d1f5e51eca100e59165e176c
actboy168 <actboy168@gmail.com>
2014-01-23 14:15:36 +0800

    去掉多余的定义

--- Development/Editor/Core/YDWEBase/base/win/registry/value.h
+++ Development/Editor/Core/YDWEBase/base/win/registry/value.h
@@ -143,7 +143,6 @@ namespace base { namespace registry {
 		if (data_size > 0)
 		{
 			std::dynarray<char_type> buffer(1 + data_size / sizeof(char_type));
-			uint32_t               dw;
 
 			data_size = buffer.size() * sizeof(char_type);
 			res = traits_type::query_value(m_hkey, m_name.c_str(), dw, buffer.data(), data_size);
