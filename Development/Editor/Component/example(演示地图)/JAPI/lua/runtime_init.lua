4f342046b0b6e74957cbf5027c632ff47efb6834
actboy168 <actboy168@gmail.com>
2014-02-13 11:23:51 +0800

    增加演示

--- Development/Editor/Component/example(演示地图)/JAPI/lua/runtime_init.lua
+++ Development/Editor/Component/example(演示地图)/JAPI/lua/runtime_init.lua
@@ -29,3 +29,8 @@ end
 ---- 0不再能被隐式转换为nil，对应jass里的null请使用nil。
 --
 jass_ext.runtime.handle_level = 1
+
+-- 开启此项时(默认开启)，调用jass.xxx/japi.xxx发生崩溃时，会生产一个lua错误，并忽略这个崩溃。
+-- 你可以注册jass_ext.runtime.error_handle，来获得这个错误。
+-- 注：开启此项会略微增加运行时消耗（即使没有发生错误）。
+jass_ext.runtime.catch_crash = true
