c9d8d3346a0f70e91a14bdd5b8bbffbefe1056f4
Shawwwn <shawwwn1@gmail.com>
2014-02-14 13:08:38 -0800

    Even more attack attributes

--- Development/Editor/Plugin/Warcraft3/yd_jass_api/UnitState.cpp
+++ Development/Editor/Plugin/Warcraft3/yd_jass_api/UnitState.cpp
@@ -20,15 +20,16 @@ namespace base { namespace warcraft3 { namespace japi {
 	    // attack 1 attribute adds
 	    UNIT_STATE_ATTACK1_DAMAGE_LOSS_FACTOR   = 0x21,
 	    UNIT_STATE_ATTACK1_WEAPON_SOUND         = 0x22,
-	    UNIT_STATE_ATTACK1_WEAPON_TYPE          = 0x23,
+	    UNIT_STATE_ATTACK1_ATTACK_TYPE          = 0x23,
 	    UNIT_STATE_ATTACK1_MAX_TARGETS          = 0x24,
 	    UNIT_STATE_ATTACK1_INTERVAL             = 0x25,
 	    UNIT_STATE_ATTACK1_INITIAL_DELAY        = 0x26,
-		UNIT_STATE_ATTACK1_BACK_SWING			= 0x28,
+	    UNIT_STATE_ATTACK1_BACK_SWING           = 0x28,
 	    UNIT_STATE_ATTACK1_RANGE_BUFFER         = 0x27,
-		UNIT_STATE_ATTACK1_TARGET_TYPES         = 0x29,
-		UNIT_STATE_ATTACK1_SPILL_DIST			= 0x56,
-		UNIT_STATE_ATTACK1_SPILL_RADIUS			= 0x57,
+	    UNIT_STATE_ATTACK1_TARGET_TYPES         = 0x29,
+	    UNIT_STATE_ATTACK1_SPILL_DIST           = 0x56,
+	    UNIT_STATE_ATTACK1_SPILL_RADIUS         = 0x57,
+		UNIT_STATE_ATTACK1_WEAPON_TYPE			= 0x58,
 	    // attack 2 attributes (sorted in a sequencial order based on memory address)
 	    UNIT_STATE_ATTACK2_DAMAGE_DICE          = 0x30,
 	    UNIT_STATE_ATTACK2_DAMAGE_SIDE          = 0x31,
@@ -36,7 +37,7 @@ namespace base { namespace warcraft3 { namespace japi {
 	    UNIT_STATE_ATTACK2_DAMAGE_BONUS         = 0x33,
 	    UNIT_STATE_ATTACK2_DAMAGE_LOSS_FACTOR   = 0x34,
 	    UNIT_STATE_ATTACK2_WEAPON_SOUND         = 0x35,
-	    UNIT_STATE_ATTACK2_WEAPON_TYPE          = 0x36,
+	    UNIT_STATE_ATTACK2_ATTACK_TYPE          = 0x36,
 	    UNIT_STATE_ATTACK2_MAX_TARGETS          = 0x37,
 	    UNIT_STATE_ATTACK2_INTERVAL             = 0x38,
 	    UNIT_STATE_ATTACK2_INITIAL_DELAY        = 0x39,
@@ -44,10 +45,11 @@ namespace base { namespace warcraft3 { namespace japi {
 	    UNIT_STATE_ATTACK2_RANGE_BUFFER         = 0x41,
 	    UNIT_STATE_ATTACK2_DAMAGE_MIN           = 0x42,
 	    UNIT_STATE_ATTACK2_DAMAGE_MAX           = 0x43,
-		UNIT_STATE_ATTACK2_BACK_SWING			= 0x44,
-		UNIT_STATE_ATTACK2_TARGET_TYPES         = 0x45,
-		UNIT_STATE_ATTACK2_SPILL_DIST			= 0x46,
-		UNIT_STATE_ATTACK2_SPILL_RADIUS			= 0x47,
+	    UNIT_STATE_ATTACK2_BACK_SWING           = 0x44,
+	    UNIT_STATE_ATTACK2_TARGET_TYPES         = 0x45,
+	    UNIT_STATE_ATTACK2_SPILL_DIST           = 0x46,
+	    UNIT_STATE_ATTACK2_SPILL_RADIUS         = 0x47,
+		UNIT_STATE_ATTACK2_WEAPON_TYPE			= 0x59,
 	    // general attributes
 	    UNIT_STATE_ARMOR_TYPE                   = 0x50,
 	    UNIT_STATE_RATE_OF_FIRE                 = 0x51, // global attack rate of unit, work on both attacks
@@ -57,7 +59,7 @@ namespace base { namespace warcraft3 { namespace japi {
 	    UNIT_STATE_MAX_LIFE                     = 0x1,
 	    UNIT_STATE_MAX_MANA                     = 0x3,
 	    UNIT_STATE_MIN_RANGE                    = 0x55,
-		// ...starts from 0x58
+	    // ...starts from 0x5A
 	};
 
 	struct unit_property {
@@ -74,18 +76,21 @@ namespace base { namespace warcraft3 { namespace japi {
 	    int32_t  attack1_damage_bonus;          // 0xAC (integer)
 	    int32_t  attack2_damage_bonus;          // +4
 	    uint32_t unk_b4[2];
-	    uint32_t  attack1_damage_loss_factor;   // 0xBC (real)
+	    uint32_t attack1_damage_loss_factor;    // 0xBC (real)
 	    uint32_t unk_c0[3];
-	    uint32_t  attack2_damage_loss_factor;   // +16(0xF)
-	    uint32_t unk_d0[6];
+	    uint32_t attack2_damage_loss_factor;    // +16(0xF)
+	    uint32_t unk_d0[3];
+	    int32_t  attack1_weapTp;                // 0xDC (integer)
+	    int32_t  attack2_weapTp;                // +4
+	    uint32_t unk_e4;
 	    int32_t  attack1_weapon_sound;          // 0xE8 (real)
 	    int32_t  attack2_weapon_sound;          // +4
 	    uint32_t unk_f0[1];
-	    int32_t attack1_weapon_type;            // 0xF4 (integer)
-	    int32_t attack2_weapon_type;            // +4
+	    int32_t  attack1_atkType;               // 0xF4 (integer)
+	    int32_t  attack2_atkType;               // +4
 	    uint32_t unk_fc[1];
-	    int32_t attack1_target_amount;          // 0x100 (integer)
-	    int32_t attack2_target_amount;          // +4
+	    int32_t  attack1_target_amount;         // 0x100 (integer)
+	    int32_t  attack2_target_amount;         // +4
 	    uint32_t unk_108[2];
 	    uint32_t attack1_spill_dist;            // 0x110 (real)
 	    uint32_t unk_114[3];
@@ -109,8 +114,8 @@ namespace base { namespace warcraft3 { namespace japi {
 	    uint32_t unk_1a4[3];
 	    uint32_t rate_of_fire;                  // 0x1B0 (real)
 	    uint32_t unk_1b4[25];
-	    int32_t attack1_targs;                  // 0x218
-	    int32_t attack2_targs;                  // +4
+	    int32_t  attack1_targs;                 // 0x218
+	    int32_t  attack2_targs;                 // +4
 	    uint32_t unk_220[9];
 	    uint32_t acquisition_range;             // 0x244 (real)
 	    uint32_t unk_248[1];
@@ -191,7 +196,7 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK1_DAMAGE_BONUS:
 		case UNIT_STATE_ATTACK1_DAMAGE_LOSS_FACTOR:
 		case UNIT_STATE_ATTACK1_WEAPON_SOUND:
-		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
+		case UNIT_STATE_ATTACK1_ATTACK_TYPE:
 		case UNIT_STATE_ATTACK1_MAX_TARGETS:
 		case UNIT_STATE_ATTACK1_INTERVAL:
 		case UNIT_STATE_ATTACK1_INITIAL_DELAY:
@@ -203,6 +208,7 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK1_TARGET_TYPES:
 		case UNIT_STATE_ATTACK1_SPILL_DIST:
 		case UNIT_STATE_ATTACK1_SPILL_RADIUS:
+		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
 
 		case UNIT_STATE_ATTACK2_DAMAGE_DICE:
 		case UNIT_STATE_ATTACK2_DAMAGE_SIDE:
@@ -210,7 +216,7 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK2_DAMAGE_BONUS:
 		case UNIT_STATE_ATTACK2_DAMAGE_LOSS_FACTOR:
 		case UNIT_STATE_ATTACK2_WEAPON_SOUND:
-		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
+		case UNIT_STATE_ATTACK2_ATTACK_TYPE:
 		case UNIT_STATE_ATTACK2_MAX_TARGETS:
 		case UNIT_STATE_ATTACK2_INTERVAL:
 		case UNIT_STATE_ATTACK2_INITIAL_DELAY:
@@ -222,6 +228,7 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK2_TARGET_TYPES:
 		case UNIT_STATE_ATTACK2_SPILL_DIST:
 		case UNIT_STATE_ATTACK2_SPILL_RADIUS:
+		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
 
 		case UNIT_STATE_ARMOR:
 		case UNIT_STATE_ARMOR_TYPE:
@@ -297,8 +304,8 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK1_WEAPON_SOUND:
 			retval = ptr->attack1_weapon_sound;
 			break;
-		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
-			retval = ptr->attack1_weapon_type;
+		case UNIT_STATE_ATTACK1_ATTACK_TYPE:
+			retval = ptr->attack1_atkType;
 			break;
 		case UNIT_STATE_ATTACK1_MAX_TARGETS:
 			retval = ptr->attack1_target_amount;
@@ -325,6 +332,9 @@ namespace base { namespace warcraft3 { namespace japi {
 			return ptr->attack1_spill_dist;
 		case UNIT_STATE_ATTACK1_SPILL_RADIUS:
 			return ptr->attack1_spill_radius;
+		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
+			retval = ptr->attack1_weapTp;
+			break;
 		// Attack - 2
 		case UNIT_STATE_ATTACK2_DAMAGE_DICE:
 			retval = ptr->attack2_damage_dice;
@@ -343,8 +353,8 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK2_WEAPON_SOUND:
 			retval = ptr->attack2_weapon_sound;
 			break;
-		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
-			retval = ptr->attack2_weapon_type;
+		case UNIT_STATE_ATTACK2_ATTACK_TYPE:
+			retval = ptr->attack2_atkType;
 			break;
 		case UNIT_STATE_ATTACK2_MAX_TARGETS:
 			retval = ptr->attack2_target_amount;
@@ -367,10 +377,14 @@ namespace base { namespace warcraft3 { namespace japi {
 			return ptr->attack2_back_swing;
 		case UNIT_STATE_ATTACK2_TARGET_TYPES:
 			retval = ptr->attack2_targs;
+			break;
 		case UNIT_STATE_ATTACK2_SPILL_DIST:
 			return ptr->attack2_spill_dist;
 		case UNIT_STATE_ATTACK2_SPILL_RADIUS:
 			return ptr->attack2_spill_radius;
+		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
+			retval = ptr->attack2_weapTp;
+			break;
 		default:
 			retval = 0;
 			break;
@@ -384,7 +398,7 @@ namespace base { namespace warcraft3 { namespace japi {
 	{
 		switch (state_type)
 		{
-		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
+		case UNIT_STATE_ATTACK1_ATTACK_TYPE:
 		case UNIT_STATE_ATTACK1_DAMAGE_DICE:
 		case UNIT_STATE_ATTACK1_DAMAGE_SIDE:
 		case UNIT_STATE_ATTACK1_DAMAGE_BASE:
@@ -402,6 +416,7 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK1_TARGET_TYPES:
 		case UNIT_STATE_ATTACK1_SPILL_DIST:
 		case UNIT_STATE_ATTACK1_SPILL_RADIUS:
+		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
 
 		case UNIT_STATE_ATTACK2_DAMAGE_DICE:
 		case UNIT_STATE_ATTACK2_DAMAGE_SIDE:
@@ -409,7 +424,7 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK2_DAMAGE_BONUS:
 		case UNIT_STATE_ATTACK2_DAMAGE_LOSS_FACTOR:
 		case UNIT_STATE_ATTACK2_WEAPON_SOUND:
-		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
+		case UNIT_STATE_ATTACK2_ATTACK_TYPE:
 		case UNIT_STATE_ATTACK2_MAX_TARGETS:
 		case UNIT_STATE_ATTACK2_INTERVAL:
 		case UNIT_STATE_ATTACK2_INITIAL_DELAY:
@@ -421,6 +436,7 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK2_TARGET_TYPES:
 		case UNIT_STATE_ATTACK2_SPILL_DIST:
 		case UNIT_STATE_ATTACK2_SPILL_RADIUS:
+		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
 
 		case UNIT_STATE_ARMOR:
 		case UNIT_STATE_ARMOR_TYPE:
@@ -518,8 +534,8 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK1_WEAPON_SOUND:
 			ptr->attack1_weapon_sound = (uint32_t)jass::from_real(*value_ptr);
 			return ;
-		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
-			ptr->attack1_weapon_type = (uint32_t)jass::from_real(*value_ptr);
+		case UNIT_STATE_ATTACK1_ATTACK_TYPE:
+			ptr->attack1_atkType = (uint32_t)jass::from_real(*value_ptr);
 			return ;
 		case UNIT_STATE_ATTACK1_MAX_TARGETS:
 			ptr->attack1_target_amount = (uint32_t)jass::from_real(*value_ptr);
@@ -548,6 +564,9 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK1_SPILL_RADIUS:
 			ptr->attack1_spill_radius = *value_ptr;
 			return ;
+		case UNIT_STATE_ATTACK1_WEAPON_TYPE:
+			ptr->attack1_weapTp = (uint32_t)jass::from_real(*value_ptr);
+			return;
 		// Attack - 2
 		case UNIT_STATE_ATTACK2_DAMAGE_DICE:
 			ptr->attack2_damage_dice = (uint32_t)jass::from_real(*value_ptr);
@@ -567,8 +586,8 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK2_WEAPON_SOUND:
 			ptr->attack2_weapon_sound = (uint32_t)jass::from_real(*value_ptr);
 			return ;
-		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
-			ptr->attack2_weapon_type = (uint32_t)jass::from_real(*value_ptr);
+		case UNIT_STATE_ATTACK2_ATTACK_TYPE:
+			ptr->attack2_atkType = (uint32_t)jass::from_real(*value_ptr);
 			return ;
 		case UNIT_STATE_ATTACK2_MAX_TARGETS:
 			ptr->attack2_target_amount = (uint32_t)jass::from_real(*value_ptr);
@@ -597,6 +616,9 @@ namespace base { namespace warcraft3 { namespace japi {
 		case UNIT_STATE_ATTACK2_SPILL_RADIUS:
 			ptr->attack2_spill_radius = *value_ptr;
 			return ;
+		case UNIT_STATE_ATTACK2_WEAPON_TYPE:
+			ptr->attack2_weapTp = (uint32_t)jass::from_real(*value_ptr);
+			return;
 		case UNIT_STATE_ATTACK1_DAMAGE_MIN:
 		case UNIT_STATE_ATTACK1_DAMAGE_MAX:
 		case UNIT_STATE_ATTACK2_DAMAGE_MIN:
