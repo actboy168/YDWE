4f2b2098d24826388dbbf683f13b8b5518d59e49
actboy168 <actboy168@gmail.com>
2014-02-14 15:16:48 +0800

    移动publish目录

--- Build/Python/build.py
+++ Build/Python/build.py
@@ -30,7 +30,7 @@ def build_clear(configuration):
     fs.remove_all(path['ResultRoot'] / 'bin' / configuration)
     fs.remove_all(path['ResultRoot'] / 'obj' / configuration)
     fs.remove_all(path['ResultRoot'] / 'lib' / configuration)
-    fs.remove_all(path['ResultRoot'] / 'publish' / configuration)
+    fs.remove_all(path['ProjectRoot'] / 'Build' / 'publish' / configuration)
     fs.remove_all(path['Development'] / 'Editor' / 'UI' / 'bin')
     fs.remove_all(path['Development'] / 'Editor' / 'UI' / 'out')
     fs.remove_all(path['Development'] / 'Editor' / 'UI' / 'txt')
@@ -39,7 +39,7 @@ def build_move(configuration):
     print ('build_move')
     fs.copy_directory(
           path['ResultRoot'] / 'bin' / configuration
-        , path['ResultRoot'] / 'publish' / configuration
+        , path['ProjectRoot'] / 'Build' / 'publish' / configuration
         , ['.pdb', '.exp', '.ilk', '.aps', '.lib']
         , False)
     
