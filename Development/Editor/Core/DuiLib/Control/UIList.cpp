7ef5ecb327a2e78fc29e3f85eaaeee1f01c4c00a
actboy168 <actboy168@gmail.com>
2014-02-21 13:34:19 +0800

    使用base库的format

--- Development/Editor/Core/DuiLib/Control/UIList.cpp
+++ Development/Editor/Core/DuiLib/Control/UIList.cpp
@@ -1,5 +1,5 @@
 #include "StdAfx.h"
-#include "../Utils/format.h"
+#include <base/util/format.h>
 
 namespace DuiLib {
 
@@ -1320,7 +1320,7 @@ void CListHeaderItemUI::PaintStatusImage(HDC hDC)
         rcThumb.bottom -= m_rcItem.top;
 
         if (!DrawImage(hDC, *m_sSepImage.get(), 
-			util::format(L"dest='%d,%d,%d,%d'", rcThumb.left, rcThumb.top, rcThumb.right, rcThumb.bottom).c_str()))
+			base::format(L"dest='%d,%d,%d,%d'", rcThumb.left, rcThumb.top, rcThumb.right, rcThumb.bottom).c_str()))
 		{
 			m_sSepImage.reset();
 		}
