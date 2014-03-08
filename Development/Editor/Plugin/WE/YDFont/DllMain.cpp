b69e43b4e37cd4e3d70c4f64970a4bbf0666cf8c
actboy168 <actboy168@gmail.com>
2014-03-08 21:48:04 +0800

    去掉测试代码

--- Development/Editor/Plugin/WE/YDFont/DllMain.cpp
+++ Development/Editor/Plugin/WE/YDFont/DllMain.cpp
@@ -7,7 +7,6 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID pReserved)
 	if (reason == DLL_PROCESS_ATTACH)
 	{
 		::DisableThreadLibraryCalls(module);
-		MessageBox(0, 0, 0, 0);
 		g_fontptr.reset(new FontManager(base::path::self().parent_path()));
 	}
 	else if (reason == DLL_PROCESS_DETACH)
