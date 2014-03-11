70dd9796702d25dee736da903df96ebeaf411dd1
actboy168 <actboy168@gmail.com>
2014-03-11 15:57:07 +0800

    注释现在还不用的代码

--- Development/Editor/Core/ydbase/base/warcraft3/virtual_mpq.cpp
+++ Development/Editor/Core/ydbase/base/warcraft3/virtual_mpq.cpp
@@ -3,7 +3,6 @@
 #include <base/file/stream.h>
 #include <base/hook/iat.h>
 #include <base/hook/inline.h>
-#include <base/util/console.h>
 #include <base/util/foreach.h>
 #include <boost/filesystem.hpp>
 #include <boost/optional.hpp>
@@ -116,60 +115,60 @@ namespace base { namespace warcraft3 { namespace virtual_mpq {
 
 	namespace fake
 	{
-		bool __stdcall SFileEnableDirectAccess(uint32_t flags)
-		{
-			return base::std_call<bool>(real::SFileEnableDirectAccess, flags);
-		}
-
-		bool __stdcall SFileGetFileArchive(HANDLE file_handle, HANDLE* mpq_handle_ptr)
-		{
-			return base::std_call<bool>(real::SFileGetFileArchive, file_handle, mpq_handle_ptr);
-		}
-
-		LCID __stdcall SFileSetLocale(LCID locale)
-		{
-			return base::std_call<LCID>(real::SFileSetLocale, locale);
-		}
-
-		bool __stdcall SFileOpenArchive(const char* mpqname, uint32_t priority, uint32_t flags, HANDLE* mpq_handle_ptr)
-		{
-			return base::std_call<bool>(real::SFileOpenArchive, mpqname, priority, flags, mpq_handle_ptr);;
-		}
-
-		bool __stdcall SFileOpenFile(const char* filename, HANDLE* file_handle_ptr)
-		{
-			return base::std_call<bool>(real::SFileOpenFile, filename, file_handle_ptr);;
-		}
-
-		bool __stdcall SFileOpenFileEx(HANDLE mpq_handle, const char *filename, uint32_t search_scope, HANDLE* file_handle_ptr)
-		{
-			return base::std_call<bool>(real::SFileOpenFileEx, mpq_handle, filename, search_scope, file_handle_ptr);;
-		}
-
-		bool __stdcall SFileCloseFile(HANDLE file_handle)
-		{
-			return base::std_call<bool>(real::SFileCloseFile, file_handle);
-		}
-
-		uint32_t __stdcall SFileGetFileSize(HANDLE file_handle, uint32_t* file_size_high_ptr)
-		{
-			return base::std_call<uint32_t>(real::SFileGetFileSize, file_handle, file_size_high_ptr);
-		}
-
-		bool __stdcall SFileReadFile(HANDLE file_handle, void* buffer, uint32_t size, uint32_t* size_ptr, uint32_t unk)
-		{
-			return base::std_call<bool>(real::SFileReadFile, file_handle, buffer, size, size_ptr, unk);
-		}
-
-		uint32_t __stdcall SFileSetFilePointer(HANDLE file_handle, uint32_t pos, uint32_t* pos_high, uint32_t method)
-		{
-			return base::std_call<uint32_t>(real::SFileSetFilePointer, file_handle, pos, pos_high, method);
-		}
-
-		bool __stdcall SFileGetArchiveName(HANDLE mpq_handle, char* name, uint32_t size)
-		{
-			return base::std_call<bool>(real::SFileGetArchiveName, mpq_handle, name, size);
-		}
+		//bool __stdcall SFileEnableDirectAccess(uint32_t flags)
+		//{
+		//	return base::std_call<bool>(real::SFileEnableDirectAccess, flags);
+		//}
+		//
+		//bool __stdcall SFileGetFileArchive(HANDLE file_handle, HANDLE* mpq_handle_ptr)
+		//{
+		//	return base::std_call<bool>(real::SFileGetFileArchive, file_handle, mpq_handle_ptr);
+		//}
+		//
+		//LCID __stdcall SFileSetLocale(LCID locale)
+		//{
+		//	return base::std_call<LCID>(real::SFileSetLocale, locale);
+		//}
+		//
+		//bool __stdcall SFileOpenArchive(const char* mpqname, uint32_t priority, uint32_t flags, HANDLE* mpq_handle_ptr)
+		//{
+		//	return base::std_call<bool>(real::SFileOpenArchive, mpqname, priority, flags, mpq_handle_ptr);;
+		//}
+		//
+		//bool __stdcall SFileOpenFile(const char* filename, HANDLE* file_handle_ptr)
+		//{
+		//	return base::std_call<bool>(real::SFileOpenFile, filename, file_handle_ptr);;
+		//}
+		//
+		//bool __stdcall SFileOpenFileEx(HANDLE mpq_handle, const char *filename, uint32_t search_scope, HANDLE* file_handle_ptr)
+		//{
+		//	return base::std_call<bool>(real::SFileOpenFileEx, mpq_handle, filename, search_scope, file_handle_ptr);;
+		//}
+		//
+		//bool __stdcall SFileCloseFile(HANDLE file_handle)
+		//{
+		//	return base::std_call<bool>(real::SFileCloseFile, file_handle);
+		//}
+		//
+		//uint32_t __stdcall SFileGetFileSize(HANDLE file_handle, uint32_t* file_size_high_ptr)
+		//{
+		//	return base::std_call<uint32_t>(real::SFileGetFileSize, file_handle, file_size_high_ptr);
+		//}
+		//
+		//bool __stdcall SFileReadFile(HANDLE file_handle, void* buffer, uint32_t size, uint32_t* size_ptr, uint32_t unk)
+		//{
+		//	return base::std_call<bool>(real::SFileReadFile, file_handle, buffer, size, size_ptr, unk);
+		//}
+		//
+		//uint32_t __stdcall SFileSetFilePointer(HANDLE file_handle, uint32_t pos, uint32_t* pos_high, uint32_t method)
+		//{
+		//	return base::std_call<uint32_t>(real::SFileSetFilePointer, file_handle, pos, pos_high, method);
+		//}
+		//
+		//bool __stdcall SFileGetArchiveName(HANDLE mpq_handle, char* name, uint32_t size)
+		//{
+		//	return base::std_call<bool>(real::SFileGetArchiveName, mpq_handle, name, size);
+		//}
 
 		bool __stdcall SFileLoadFile(const char* filename, const void** buffer_ptr, uint32_t* size_ptr, uint32_t reserve_size, OVERLAPPED* overlapped_ptr)
 		{
@@ -216,20 +215,20 @@ namespace base { namespace warcraft3 { namespace virtual_mpq {
 			return base::std_call<bool>(real::SFileFileExistsEx, mpq_handle, filename, flags);
 		}
 
-		bool __stdcall SFileReadFileEx(HANDLE file_handle, void* buffer, uint32_t size, uint32_t* size_ptr, uint32_t unk, OVERLAPPED* overlapped_ptr, uint32_t unk2)
-		{
-			return base::std_call<bool>(real::SFileReadFileEx, file_handle, buffer, size, size_ptr, unk, overlapped_ptr, unk2);
-		}
-
-		bool __stdcall SFileOpenFileAsArchive(HANDLE handle, const char* filename, uint32_t priority, uint32_t flags, HANDLE* mpq_handle_ptr)
-		{
-			return base::std_call<bool>(real::SFileOpenFileAsArchive, handle, filename, priority, flags, mpq_handle_ptr);
-		}
-
-		bool __stdcall SFileOpenPathAsArchive(HANDLE handle, const char* pathname, uint32_t priority, uint32_t flags, HANDLE* mpq_handle_ptr)
-		{
-			return base::std_call<bool>(real::SFileOpenPathAsArchive, handle, pathname, priority, flags, mpq_handle_ptr);
-		}
+		//bool __stdcall SFileReadFileEx(HANDLE file_handle, void* buffer, uint32_t size, uint32_t* size_ptr, uint32_t unk, OVERLAPPED* overlapped_ptr, uint32_t unk2)
+		//{
+		//	return base::std_call<bool>(real::SFileReadFileEx, file_handle, buffer, size, size_ptr, unk, overlapped_ptr, unk2);
+		//}
+		//
+		//bool __stdcall SFileOpenFileAsArchive(HANDLE handle, const char* filename, uint32_t priority, uint32_t flags, HANDLE* mpq_handle_ptr)
+		//{
+		//	return base::std_call<bool>(real::SFileOpenFileAsArchive, handle, filename, priority, flags, mpq_handle_ptr);
+		//}
+		//
+		//bool __stdcall SFileOpenPathAsArchive(HANDLE handle, const char* pathname, uint32_t priority, uint32_t flags, HANDLE* mpq_handle_ptr)
+		//{
+		//	return base::std_call<bool>(real::SFileOpenPathAsArchive, handle, pathname, priority, flags, mpq_handle_ptr);
+		//}
 	}
 
 	void open_path(const boost::filesystem::path& p, uint32_t priority)
@@ -242,25 +241,25 @@ namespace base { namespace warcraft3 { namespace virtual_mpq {
 		real::SMemAlloc = (uintptr_t)::GetProcAddress(::GetModuleHandleW(L"Storm.dll"), (const char*)401);
 
 #define IAT_HOOK(ord, name) real:: ## name = base::hook::iat(module_handle, "Storm.dll", (const char*)(ord), (uintptr_t)fake:: ## name ##)
-		IAT_HOOK(253, SFileCloseFile);
-		IAT_HOOK(263, SFileEnableDirectAccess);
-		IAT_HOOK(264, SFileGetFileArchive);
-		IAT_HOOK(265, SFileGetFileSize);
-		IAT_HOOK(266, SFileOpenArchive);
-		IAT_HOOK(267, SFileOpenFile);
-		IAT_HOOK(268, SFileOpenFileEx);
-		IAT_HOOK(269, SFileReadFile);
-		IAT_HOOK(271, SFileSetFilePointer);
-		IAT_HOOK(272, SFileSetLocale);
-		IAT_HOOK(275, SFileGetArchiveName);
+		//IAT_HOOK(253, SFileCloseFile);
+		//IAT_HOOK(263, SFileEnableDirectAccess);
+		//IAT_HOOK(264, SFileGetFileArchive);
+		//IAT_HOOK(265, SFileGetFileSize);
+		//IAT_HOOK(266, SFileOpenArchive);
+		//IAT_HOOK(267, SFileOpenFile);
+		//IAT_HOOK(268, SFileOpenFileEx);
+		//IAT_HOOK(269, SFileReadFile);
+		//IAT_HOOK(271, SFileSetFilePointer);
+		//IAT_HOOK(272, SFileSetLocale);
+		//IAT_HOOK(275, SFileGetArchiveName);
 		IAT_HOOK(279, SFileLoadFile);
 		IAT_HOOK(280, SFileUnloadFile);
 		IAT_HOOK(281, SFileLoadFileEx);
 		IAT_HOOK(288, SFileFileExists);
 		IAT_HOOK(289, SFileFileExistsEx);
-		IAT_HOOK(291, SFileReadFileEx);
-		IAT_HOOK(293, SFileOpenFileAsArchive);
-		IAT_HOOK(300, SFileOpenPathAsArchive);
+		//IAT_HOOK(291, SFileReadFileEx);
+		//IAT_HOOK(293, SFileOpenFileAsArchive);
+		//IAT_HOOK(300, SFileOpenPathAsArchive);
 #undef 	IAT_HOOK
 	}
 }}}
