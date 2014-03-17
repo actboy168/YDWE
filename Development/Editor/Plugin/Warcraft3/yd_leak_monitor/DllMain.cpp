4176c27537d5e96b9f63c156c73c24df657b287a
actboy168 <actboy168@gmail.com>
2014-03-17 23:01:28 +0800

    修正handle_table计算错误的问题

--- Development/Editor/Plugin/Warcraft3/yd_leak_monitor/DllMain.cpp
+++ Development/Editor/Plugin/Warcraft3/yd_leak_monitor/DllMain.cpp
@@ -175,7 +175,7 @@ void create_report(std::fstream& fs)
 
 	using namespace base::warcraft3;
 
-	hashtable::reverse_table* table = get_jass_vm()->handle_table->table;
+	hashtable::reverse_table* table = &((*get_jass_vm()->handle_table)->table);
 	for (auto it = table->begin(); it != table->end(); ++it)
 	{
 		ht.add_handle(it->index_, (uint32_t)table->at(3 * (it->index_ - 0x100000) + 1), (uint32_t)table->at(3 * (it->index_ - 0x100000)));
