d1d807380611db4ed7e59621b8302d5f805ccdbc
actboy168 <actboy168@gmail.com>
2014-03-29 04:59:42 +0800

    增加一种新的hook方式

---
+++ Development/Editor/Core/ydbase/base/hook/dyn_iat.cpp
@@ -0,0 +1,127 @@
+#include <base/hook/dyn_iat.h>
+#include <base/hook/fp_call.h>
+#include <base/hook/iat.h>
+#include <Windows.h>
+#include <map>
+#include <string>
+
+namespace base { namespace hook {
+
+	namespace real
+	{
+		uintptr_t getprocaddressa = 0;
+	}
+
+	namespace fake
+	{
+		struct hookfunction_t
+		{
+			uintptr_t  new_func;
+			uintptr_t* old_func;
+
+			void add_funtion(uintptr_t new_function, uintptr_t* old_function)
+			{
+				new_func = new_function;
+				old_func = old_function;
+			}
+
+			uintptr_t try_hook(uintptr_t old_funcion)
+			{
+				if (old_func)
+				{
+					*old_func = old_funcion;
+				}
+				return new_func;
+			}
+		};
+
+		struct dllfunction_t
+		{
+			std::map<std::string, hookfunction_t> func_by_name;
+			std::map<uintptr_t, hookfunction_t>   func_by_ord;
+
+			void add_funtion(const char* api_name, uintptr_t new_function, uintptr_t* old_function)
+			{
+				if ((uintptr_t)api_name >> 16)
+				{
+					func_by_name[api_name].add_funtion(new_function, old_function);
+				}
+				else
+				{
+					func_by_ord[(uintptr_t)api_name].add_funtion(new_function, old_function);
+				}
+			}
+
+			uintptr_t try_hook(const char* api_name, uintptr_t old_funcion)
+			{
+				if ((uintptr_t)api_name >> 16)
+				{
+					auto it = func_by_name.find(api_name);
+					if (it != func_by_name.end())
+					{
+						return it->second.try_hook(old_funcion);
+					}
+				}
+				else
+				{
+					auto it = func_by_ord.find((uintptr_t)api_name);
+					if (it != func_by_ord.end())
+					{
+						return it->second.try_hook(old_funcion);
+					}
+				}
+				return old_funcion;
+			}
+		};
+
+		struct allfunction_t
+			: public std::map<std::wstring, dllfunction_t>
+		{
+			void add_funtion(const wchar_t* dll_name, const char* api_name, uintptr_t new_function, uintptr_t* old_function)
+			{
+				(*this)[dll_name].add_funtion(api_name, new_function, old_function);
+			}
+
+			uintptr_t try_hook(HMODULE dll_handle, const char* api_name, uintptr_t old_funcion)
+			{
+				for (auto it = begin(); it != end(); ++it)
+				{
+					if (dll_handle == GetModuleHandleW(it->first.c_str()))
+					{
+						uintptr_t result = it->second.try_hook(api_name, old_funcion);
+						if (result)
+						{
+							return result;
+						}
+					}
+				}
+				return old_funcion;
+			}
+		};
+
+		allfunction_t dyniatfunc;
+
+		uintptr_t __stdcall getprocaddressa(HMODULE lib, const char* name)
+		{
+			uintptr_t old_funcion = std_call<uintptr_t>(getprocaddressa, name);
+			return dyniatfunc.try_hook(lib, name, old_funcion);
+		}
+	}
+
+	bool dyn_iat(HMODULE module_handle, const wchar_t* dll_name, const char* api_name, uintptr_t new_function, uintptr_t* old_function)
+	{
+		fake::dyniatfunc.add_funtion(dll_name, api_name, new_function, old_function);
+
+		if (real::getprocaddressa == 0)
+		{
+			real::getprocaddressa = iat(module_handle, "kernel32.dll", "GetProcAddressA", (uintptr_t)fake::getprocaddressa);
+		}
+
+		return true;
+	}
+
+	bool dyn_iat(const wchar_t* module_name, const wchar_t* dll_name, const char* api_name, uintptr_t new_function, uintptr_t* old_function)
+	{
+		return dyn_iat(::GetModuleHandleW(module_name), dll_name, api_name, new_function, old_function);
+	}
+}}
