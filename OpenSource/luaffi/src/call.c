67b5b1f86faf5217fbea088b7f841e03ea22cd9e
actboy168 <actboy168@gmail.com>
2014-12-09 22:21:54 +0800

    支持5.3

--- OpenSource/luaffi/src/call.c
+++ OpenSource/luaffi/src/call.c
@@ -178,6 +178,8 @@ static void* reserve_code(struct jit* jit, lua_State* L, size_t sz)
         ADDFUNC(jit->lua_dll, lua_settop);
 #if LUA_VERSION_NUM < 503
         ADDFUNC(jit->lua_dll, lua_remove);
+#else		 
+		ADDFUNC(jit->lua_dll, lua_rotate);
 #endif
 #undef ADDFUNC
 
