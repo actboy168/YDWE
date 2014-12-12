b76a5c98d2f8336c0db582b5c59378beec214e72
actboy168 <actboy168@gmail.com>
2014-12-12 22:36:58 +0800

    修正一个编译错误

--- Development/Editor/Plugin/Warcraft3/yd_loader/DllMain.cpp
+++ Development/Editor/Plugin/Warcraft3/yd_loader/DllMain.cpp
@@ -7,6 +7,7 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID /*pReserved*/)
 {
 	if (reason == DLL_PROCESS_ATTACH)
 	{
+		MessageBoxA(0, 0, 0, 0);
 		DisableThreadLibraryCalls(module);
 		g_DllMod.Attach();
 	}
