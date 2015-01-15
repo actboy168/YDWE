2433fd37e275fb4b0888b0f4362a04ed9f03761e
actboy168 <actboy168@gmail.com>
2015-01-15 17:27:56 +0800

    registry v2

---
+++ Development/Editor/Core/ydbase/base/win/registry/v2/predefined_keys.h
@@ -0,0 +1,45 @@
+#pragma once
+
+namespace base { namespace registry { namespace v2 {
+
+	class predefined_key
+	{
+	public:
+		typedef HKEY   hkey_type;
+
+	public:
+		predefined_key(hkey_type hkey)
+			: m_hkey(hkey)
+		{ }
+
+		hkey_type handle() const
+		{
+			return m_hkey;
+		}
+
+	private:
+		hkey_type m_hkey;
+	};
+
+	template <typename C>
+	inline basic_key<C> operator/(const predefined_key& lhs, const std::basic_string<C>& rhs)
+	{
+		return basic_key<C>(lhs.handle(), rhs);
+	}
+
+	template <typename C>
+	inline basic_key<C> operator/(const predefined_key& lhs, const C* rhs)
+	{
+		return basic_key<C>(lhs.handle(), rhs);
+	}
+
+	inline predefined_key current_user()
+	{
+		return predefined_key(HKEY_CURRENT_USER);
+	}
+
+	inline predefined_key local_machine()
+	{
+		return predefined_key(HKEY_LOCAL_MACHINE);
+	}
+}}}
