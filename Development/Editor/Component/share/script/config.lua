582bf30bc93b809c7a619a8910b1c9e095fd43e3
最萌小汐 <sunyijiusyj@126.com>
2015-02-07 01:53:16 +0800

    修正颜色插件没有默认开启的BUG

--- Development/Editor/Component/share/script/config.lua
+++ Development/Editor/Component/share/script/config.lua
@@ -4,43 +4,43 @@ local file = fs.ydwe_path() / "bin" / "EverConfig.cfg"
 local tbl = nil
 local tbl_index = {
 	MapSave = {
-		Option = 0
+		Option = "0"
 	},
 	War3Patch = {
-		Option = 0
+		Option = "0"
 	},
 	MapTest = {
 		LaunchRenderingEngine = "Direct3D 8",
-		LaunchWindowed = 1,
-		LaunchFullWindowed = 0,
-		LaunchLockingMouse = 0,
-		LaunchFixedRatioWindowed = 1,
-		LaunchDisableSecurityAccess = 0,
+		LaunchWindowed = "1",
+		LaunchFullWindowed = "0",
+		LaunchLockingMouse = "0",
+		LaunchFixedRatioWindowed = "1",
+		LaunchDisableSecurityAccess = "0",
 	},
 	ScriptCompiler = {
-		EnableJassHelper = 1,
-		EnableJassHelperDebug = 0,
-		EnableJassHelperScriptOnly = 0,
-		EnableJassHelperOptimization = 1,
-		EnableCJass = 0,
+		EnableJassHelper = "1",
+		EnableJassHelperDebug = "0",
+		EnableJassHelperScriptOnly = "0",
+		EnableJassHelperOptimization = "1",
+		EnableCJass = "0",
 	},
 	ScriptInjection = {
-		Option = 0
+		Option = "0"
 	},
 	ThirdPartyPlugin = {
-		EnableDotNetSupport = 1,
-		EnableTesh = 1,
-		EnableYDTrigger = 1,
+		EnableDotNetSupport = "1",
+		EnableTesh = "1",
+		EnableYDTrigger = "1",
 	},
 	FeatureToggle = {
-		EnableManualNewId = 0,
-		EnableTriggerCopyEncodingAutoConversion = 1,
-		EnableShowInternalAttributeId = 0,
+		EnableManualNewId = "0",
+		EnableTriggerCopyEncodingAutoConversion = "1",
+		EnableShowInternalAttributeId = "0",
 	},
 	Font = {
-		FontEnable = 0,
+		FontEnable = "0",
 		FontName = "",
-		FontSize = 12,
+		FontSize = "12",
 	},
 }
 
