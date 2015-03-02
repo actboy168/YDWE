3f585a67ccdd22ab9f426439d1f62ea2a6e59363
actboy168 <actboy168@gmail.com>
2015-03-02 10:03:04 +0800

    去掉LuaEngineImpl

--- Development/Editor/Core/LuaEngine/LuaEngine.h
+++ Development/Editor/Core/LuaEngine/LuaEngine.h
@@ -1,23 +1,25 @@
 #pragma once
 
 #include <boost/filesystem.hpp>
-
-class LuaEngineImpl;
+#include <lua.hpp>
+#include <../Plugin/Lua/log/logging.h>
 
 class LuaEngine
 {
 public:
 	LuaEngine();
 	~LuaEngine();
-	bool Initialize(boost::filesystem::path const& log_config);
+	bool Initialize(const boost::filesystem::path& root_path);
+	bool InitializeLogger(const boost::filesystem::path& root_path);
+	bool InitializeInfo();
+	bool InitializeLua();
 	bool Uninitialize();
 	bool SetPath(boost::filesystem::path const& path);
 	bool SetCPath(boost::filesystem::path const& cpath);
 	bool LoadFile(boost::filesystem::path const& file_path);
 
 private:
-#pragma warning(push)
-#pragma warning(disable:4251)	
-	std::unique_ptr<LuaEngineImpl> pimpl_;
-#pragma warning(pop)
+	lua_State*              state_;
+	logging::logger         logger_;
+	bool                    vaild_;
 };
