c09aa426a63272ebdc869e4dab629ac435262966
actboy168 <actboy168@gmail.com>
2015-04-07 14:30:53 +0800

    增加namespace

--- Development/Editor/Core/ydbase/base/util/horrible_cast.h
+++ Development/Editor/Core/ydbase/base/util/horrible_cast.h
@@ -1,17 +1,19 @@
 #pragma once
 
-template <class OutputClass, class InputClass>
-union horrible_union 
-{
-	OutputClass out;
-	InputClass in;
-};
-
-template <class OutputClass, class InputClass>
-inline OutputClass horrible_cast(const InputClass input)
-{
-	horrible_union<OutputClass, InputClass> u;
-	static_assert((sizeof(InputClass)==sizeof(u)) && (sizeof(InputClass)==sizeof(OutputClass)), "OutputClass and InputClass are not the same size.");
-	u.in = input;
-	return u.out;
+namespace base {
+	template <class OutputClass, class InputClass>
+	union horrible_union 
+	{
+		OutputClass out;
+		InputClass in;
+	};
+	
+	template <class OutputClass, class InputClass>
+	inline OutputClass horrible_cast(const InputClass input)
+	{
+		horrible_union<OutputClass, InputClass> u;
+		static_assert((sizeof(InputClass)==sizeof(u)) && (sizeof(InputClass)==sizeof(OutputClass)), "OutputClass and InputClass are not the same size.");
+		u.in = input;
+		return u.out;
+	}
 }
