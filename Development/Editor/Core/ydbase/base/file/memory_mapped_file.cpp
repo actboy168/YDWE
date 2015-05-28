9e82b7db44269b1a19bb7ded2eb58242b66679c1
actboy168 <actboy168@gmail.com>
2015-05-28 09:56:58 +0800

    revert

--- Development/Editor/Core/ydbase/base/file/memory_mapped_file.cpp
+++ Development/Editor/Core/ydbase/base/file/memory_mapped_file.cpp
@@ -52,11 +52,43 @@ namespace base { namespace file {
 
 	void memory_mapped_file::open(void* file, uint64_t offset, size_t request_size)
 	{
-		uint64_t map_size = offset + request_size;
+		DWORD   size_high;
+		DWORD   size_low = ::GetFileSize(file, &size_high);
+		DWORD   error_code = ::GetLastError();
+
+		if (INVALID_FILE_SIZE == size_low && ERROR_SUCCESS != error_code)
+		{
+			throw windows_exception("failed to determine file size", error_code);
+		}
+
+		uint64_t file_size = (static_cast<uint64_t>(size_high) << 32) | size_low;
+		uint64_t map_size = offset + request_size;;
 
 		if (map_size < offset)
 		{
 			throw windows_exception("requested region exceeds the available address space", ERROR_INVALID_PARAMETER);
+		}
+
+		if (offset > file_size)
+		{
+			if (0 == request_size)
+			{
+				throw windows_exception("region out of range", ERROR_INVALID_PARAMETER);
+			}
+		}
+		else if (0 == request_size)
+		{
+#ifdef _WIN64
+			request_size = file_size - offset;
+#else
+			uint64_t request_size2 = file_size - offset;
+			if (request_size2 > static_cast<uint64_t>(0xFFFFFFFF))
+			{
+				throw windows_exception("region size too large", ERROR_NOT_ENOUGH_MEMORY);
+			}
+			request_size = static_cast<size_t>(request_size2);
+#endif
+			map_size = offset + request_size;
 		}
 
 		if (0 == request_size)
