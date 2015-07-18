9ea4ea545dc1f4f54733332479d40bb76114d05d
actboy168 <actboy168@gmail.com>
2015-07-18 14:58:12 +0800

    replace_pointer不需要依赖DISABLE_DETOURS

--- Development/Editor/Core/ydbase/base/hook/inline.h
+++ Development/Editor/Core/ydbase/base/hook/inline.h
@@ -3,10 +3,10 @@
 #include <base/config.h>
 #include <cstdint>
 
+namespace base { namespace hook {  
 #if !defined(DISABLE_DETOURS)
-namespace base { namespace hook {
 	_BASE_API bool inline_install(uintptr_t* pointer_ptr, uintptr_t detour);
-	_BASE_API bool inline_uninstall(uintptr_t* pointer_ptr, uintptr_t detour);	
+	_BASE_API bool inline_uninstall(uintptr_t* pointer_ptr, uintptr_t detour);
+#endif
 	_BASE_API uintptr_t replace_pointer(uintptr_t address, uintptr_t new_value);
 }}
-#endif
