953321ef03ef655dd9da35a015016582b3056143
actboy168 <actboy168@gmail.com>
2016-03-15 15:05:39 +0800

    jass hook支持1.27a

--- Development/Editor/Core/ydbase/base/warcraft3/jass/nf_register.cpp
+++ Development/Editor/Core/ydbase/base/warcraft3/jass/nf_register.cpp
@@ -18,6 +18,36 @@ namespace base { namespace warcraft3 { namespace jass { namespace nf_register {
 	{
 		uintptr_t retval = std_call<uintptr_t>(real_storm_alloc, amount, log_filename, log_line, default_value);
 
+		if ((amount == 176)
+			&& (log_line == 668)
+			&& (default_value == 0)
+			&& (strcmp(log_filename, "Jass.cpp") == 0))
+		{
+			stat = 1;
+			thread_id = ::GetCurrentThreadId();
+		}
+		else if (stat == 3)
+		{
+			if (thread_id == ::GetCurrentThreadId())
+			{
+				if ((amount == 140)
+					&& (log_line == 90)
+					&& (default_value == 0)
+					&& (strcmp(log_filename, "Agile.cpp") == 0))
+				{
+					stat = 0;
+					event_hook(retval);
+				}
+			}
+		}
+
+		return retval;
+	}
+
+	uintptr_t __stdcall fake_storm_alloc_126(uint32_t amount, const char* log_filename, uint32_t log_line, uint32_t default_value)
+	{
+		uintptr_t retval = std_call<uintptr_t>(real_storm_alloc, amount, log_filename, log_line, default_value);
+
 		if ((amount == 176) 
 			&& (log_line == 668)
 			&& (default_value == 0)
@@ -125,9 +155,14 @@ namespace base { namespace warcraft3 { namespace jass { namespace nf_register {
 	{
 		DO_ONCE_NOTHREADSAFE()
 		{
-			if (get_war3_searcher().get_version() > version_123)
+			if (get_war3_searcher().get_version() > version_126)
 			{
-				real_storm_alloc   = hook::iat(L"Game.dll", "Storm.dll",    (const char*)401, (uintptr_t)fake_storm_alloc);
+				real_storm_alloc = hook::iat(L"Game.dll", "Storm.dll", (const char*)401, (uintptr_t)fake_storm_alloc);
+				real_tls_get_value = hook::iat(L"Game.dll", "Kernel32.dll", "TlsGetValue", (uintptr_t)fake_tls_get_value);
+			}
+			else if (get_war3_searcher().get_version() > version_123)
+			{
+				real_storm_alloc   = hook::iat(L"Game.dll", "Storm.dll",    (const char*)401, (uintptr_t)fake_storm_alloc_126);
 				real_tls_get_value = hook::iat(L"Game.dll", "Kernel32.dll", "TlsGetValue",    (uintptr_t)fake_tls_get_value);
 			}
 			else if (get_war3_searcher().get_version() > version_121b)
