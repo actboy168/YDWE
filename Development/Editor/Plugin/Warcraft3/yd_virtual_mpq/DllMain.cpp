6b81b160fbe95f951a55202367be325eccc31400
actboy168 <actboy168@gmail.com>
2016-04-05 20:47:47 +0800

    支持指定路径

--- Development/Editor/Plugin/Warcraft3/yd_virtual_mpq/DllMain.cpp
+++ Development/Editor/Plugin/Warcraft3/yd_virtual_mpq/DllMain.cpp
@@ -1,11 +1,25 @@
 #include <windows.h>
 #include <base/path/self.h>
-#include <base/warcraft3/virtual_mpq.h>
+#include <base/warcraft3/virtual_mpq.h>	
+#include <slk/reader/IniReader.hpp>
+#include <slk/reader/IniReader.cpp>
+#include <slk/reader/CommonReader.cpp> 
+#include <base/file/stream.h>
 
 void Initialize()
 {
 	base::warcraft3::virtual_mpq::initialize(::GetModuleHandleW(L"Game.dll"));
-	base::warcraft3::virtual_mpq::open_path(base::path::self().remove_filename().remove_filename().remove_filename() / L"share" / L"mpq" / L"war3", 15);
+	boost::filesystem::path ydwe_path = base::path::self().remove_filename().remove_filename().remove_filename();
+	slk::IniTable table;
+	table["MapTest"]["VirtualMpq"] = "share/mpq/war3";
+	try {
+		base::buffer buf = base::file::read_stream(ydwe_path / L"bin" / L"EverConfig.cfg").read<base::buffer>();
+		base::buffer_reader reader(buf);
+		slk::IniReader::Read(reader, table);
+	}
+	catch (...) {
+	}
+	base::warcraft3::virtual_mpq::open_path(ydwe_path / table["MapTest"]["VirtualMpq"], 15);
 }
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID pReserved)
