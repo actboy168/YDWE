08ce6b64a2df8734fac630a1e6d3c721b5a0ec7a
actboy168 <actboy168@gmail.com>
2016-04-07 22:22:25 +0800

    支持jass指定图标路径

--- Development/Editor/Plugin/Warcraft3/yd_jass_api/DisableButtonBlp.cpp
+++ Development/Editor/Plugin/Warcraft3/yd_jass_api/DisableButtonBlp.cpp
@@ -3,7 +3,10 @@
 #include <boost/filesystem.hpp>
 #include <base/path/helper.h>	
 #include <base/file/stream.h> 
-#include <base/warcraft3/virtual_mpq.h> 
+#include <base/warcraft3/virtual_mpq.h>  
+#include <base/warcraft3/jass/hook.h>	
+#include <base/warcraft3/jass.h>	
+#include <base/warcraft3/event.h>
 #include <BlpConv/BlpConv.h>
 #include <BlpConv/Blp.h>
 #include <algorithm>
@@ -125,44 +128,56 @@ namespace base { namespace warcraft3 { namespace japi {
 		}
 
 		static std::map<std::string, std::string> g_history;
-		static std::string g_lastfile;
-		bool __stdcall SFileLoadFile(const char* filename, const void** buffer_ptr, uint32_t* size_ptr, uint32_t reserve_size, OVERLAPPED* overlapped_ptr)
+		static std::string g_lastfilepath;
+		bool __stdcall SFileLoadFile(const char* filepath, const void** buffer_ptr, uint32_t* size_ptr, uint32_t reserve_size, OVERLAPPED* overlapped_ptr)
 		{
-			if (!buffer_ptr || !filename)
+			if (!buffer_ptr || !filepath)
 			{
-				return base::std_call<bool>(real::SFileLoadFile, filename, buffer_ptr, size_ptr, reserve_size, overlapped_ptr);
+				return base::std_call<bool>(real::SFileLoadFile, filepath, buffer_ptr, size_ptr, reserve_size, overlapped_ptr);
 			}
-			bool suc = base::std_call<bool>(real::SFileLoadFile, filename, buffer_ptr, size_ptr, reserve_size, overlapped_ptr);
+			bool suc = base::std_call<bool>(real::SFileLoadFile, filepath, buffer_ptr, size_ptr, reserve_size, overlapped_ptr);
 			if (suc)
 			{
-				g_lastfile = filename;
+				g_lastfilepath = filepath;
 				return true;
 			}
 #define DisString "replaceabletextures\\commandbuttonsdisabled\\dis"
-#define STRLEN(s) (sizeof(s) - 1)
-			if (0 == strnicmp(filename, DisString, STRLEN(DisString)))
+#define StrLen(s) (sizeof(s) - 1)
+			if (0 == strnicmp(filepath, DisString, StrLen(DisString)))
 			{
-				if (0 != stricmp(ToFileName(g_lastfile).c_str(), filename + STRLEN(DisString)))
+				const char* filename = filepath + StrLen(DisString);
+				if (0 != stricmp(ToFileName(g_lastfilepath).c_str(), filename))
 				{
-					if (0 == strnicmp(filename, DisString "dis", STRLEN(DisString "dis")))
+					for (const char* cur = filename;; cur += 3)
 					{
-						auto it = g_history.find(filename + STRLEN(DisString "dis"));
+						auto it = g_history.find(cur);
 						if (it != g_history.end())
 						{
 							suc = disable_button_blp(it->second.c_str(), buffer_ptr, size_ptr, reserve_size);
+							break;
+						}
+						if (0 != strnicmp(cur, "dis", 3))
+						{
+							break;
 						}
 					}
 				}
 				else
 				{
-					g_history[filename + STRLEN(DisString)] = g_lastfile;
-					suc = disable_button_blp(g_lastfile.c_str(), buffer_ptr, size_ptr, reserve_size);
+					g_history[filename] = g_lastfilepath;
+					suc = disable_button_blp(g_lastfilepath.c_str(), buffer_ptr, size_ptr, reserve_size);
 				}
 			}
-			g_lastfile = filename;
+			g_lastfilepath = filepath;
 			if (overlapped_ptr && overlapped_ptr->hEvent) ::SetEvent(overlapped_ptr->hEvent);
 			return suc;
 		}
+	}
+
+	void __cdecl EXDclareButtonIcon(jass::jstring_t path)
+	{
+		std::string str_path = jass::from_trigstring(jass::from_string(path));
+		fake::g_history[ToFileName(str_path)] = str_path;
 	}
 
 	void InitializeDisableButtonBlp()
@@ -172,6 +187,12 @@ namespace base { namespace warcraft3 { namespace japi {
 		virtual_mpq::initialize(module_handle);
 		real::SMemAlloc       = (uintptr_t)::GetProcAddress(::GetModuleHandleW(L"Storm.dll"), (const char*)401);   
 		real::SFileUnloadFile = (uintptr_t)::GetProcAddress(::GetModuleHandleW(L"Storm.dll"), (const char*)280); 
-		real::SFileLoadFile   = base::hook::iat(module_handle, "Storm.dll", (const char*)(279), (uintptr_t)fake::SFileLoadFile);
+		real::SFileLoadFile = base::hook::iat(module_handle, "Storm.dll", (const char*)(279), (uintptr_t)fake::SFileLoadFile);
+		jass::japi_add((uintptr_t)EXDclareButtonIcon, "EXDclareButtonIcon", "(S)V");
+		register_game_reset_event([&](uintptr_t)
+		{
+			fake::g_lastfilepath.clear();
+			fake::g_history.clear();
+		});
 	}
 }}}
