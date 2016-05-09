f15513a69a2a4b315af557da3bc960987a6a7d65
actboy168 <actboy168@gmail.com>
2016-05-09 10:07:27 +0800

    使用5.3的写法

--- Development/Editor/Plugin/Warcraft3/yd_lua_engine/lua_engine/fix_math.cpp
+++ Development/Editor/Plugin/Warcraft3/yd_lua_engine/lua_engine/fix_math.cpp
@@ -7,44 +7,39 @@ namespace base { namespace warcraft3 { namespace lua_engine {
 
 	int fix_math_random(lua_State* L)
 	{
-		lua_Number r;
-
+		lua_Integer low = 0, up = 1;
+		double r;
 		if (is_gaming())
 		{
 			jass::jreal_t a = jass::to_real(0.f);
 			jass::jreal_t b = jass::to_real(1.f);
-			r = (lua_Number)jass::from_real(jass::call("GetRandomReal", &a, &b));
+			r = (double)jass::from_real(jass::call("GetRandomReal", &a, &b));
 		}
 		else
 		{
-			r = (lua_Number)(rand()%RAND_MAX) / (lua_Number)RAND_MAX;
+			r = (double)rand() * (1.0 / ((double)RAND_MAX + 1.0));
 		}
 
 		switch (lua_gettop(L)) 
 		{
 		case 0:
-			{
-				lua_pushnumber(L, r);
-				break;
-			}
+			lua_pushnumber(L, (lua_Number)r);
+			break;
 		case 1:
-			{
-				lua_Number u = luaL_checknumber(L, 1);
-				luaL_argcheck(L, (lua_Number)1.0 <= u, 1, "interval is empty");
-				lua_pushnumber(L, floor(r*u) + (lua_Number)(1.0));
-				break;
-			}
+			low = 1;
+			up = luaL_checkinteger(L, 1);
+			break;
 		case 2:
-			{
-				lua_Number l = luaL_checknumber(L, 1);
-				lua_Number u = luaL_checknumber(L, 2);
-				luaL_argcheck(L, l <= u, 2, "interval is empty");
-				lua_pushnumber(L, floor(r*(u - l + 1)) + l);
-				break;
-			}
+			low = luaL_checkinteger(L, 1);
+			up = luaL_checkinteger(L, 2);
+			break;
 		default:
 			return luaL_error(L, "wrong number of arguments");
 		}
+		luaL_argcheck(L, low <= up, 1, "interval is empty");
+		luaL_argcheck(L, low >= 0 || up <= LUA_MAXINTEGER + low, 1, "interval too large");
+		r *= (double)(up - low) + 1.0;
+		lua_pushinteger(L, (lua_Integer)r + low);
 		return 1;
 	}
 
@@ -52,11 +47,11 @@ namespace base { namespace warcraft3 { namespace lua_engine {
 	{
 		if (is_gaming())
 		{
-			jass::call("SetRandomSeed", lua_tointeger(L, 1));
+			jass::call("SetRandomSeed", (unsigned int)(lua_Integer)luaL_checknumber(L, 1));
 		}
 		else
 		{
-			srand(lua_tointeger(L, 1));
+			srand((unsigned int)(lua_Integer)luaL_checknumber(L, 1));
 			(void)rand();
 		}
 
