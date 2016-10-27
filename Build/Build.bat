1fb73e2d9d35551a7dec3279b58e30a86bd1d116
actboy168 <actboy168@gmail.com>
2016-10-28 00:59:01 +0800

    试试全路径

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,4 +1,4 @@
 @echo OFF
 CD lua\luabuild\bin
-".\lua.exe" "..\..\make.lua" "%~dp0lua" %1 "%~dp0build.log" %2
+"%~dp0lua\luabuild\bin\lua.exe" "%~dp0lua\make.lua" "%~dp0lua" %1 "%~dp0build.log" %2
 CD ..\..\..
