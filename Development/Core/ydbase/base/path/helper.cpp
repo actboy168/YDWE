083973ea48cfaae4d9a3f8cf06e5874791063447
actboy168 <actboy168@gmail.com>
2016-11-18 01:08:13 +0800

    修正一个bug

--- Development/Core/ydbase/base/path/helper.cpp
+++ Development/Core/ydbase/base/path/helper.cpp
@@ -27,10 +27,10 @@ namespace base { namespace path {
 
 	bool equal(fs::path const& lhs, fs::path const& rhs)
 	{
-		fs::path lpath(lhs);
-		fs::path rpath(rhs);
-		const fs::path::value_type* l(normalize(lpath).c_str());
-		const fs::path::value_type* r(normalize(rpath).c_str());
+		fs::path lpath = normalize(lhs);
+		fs::path rpath = normalize(rhs);
+		const fs::path::value_type* l(lpath.c_str());
+		const fs::path::value_type* r(rpath.c_str());
 		while ((tolower(*l) == tolower(*r) || (*l == L'\\' && *r == L'/') || (*l == L'/' && *r == L'\\')) && *l) 
 		{ 
 			++l; ++r; 
