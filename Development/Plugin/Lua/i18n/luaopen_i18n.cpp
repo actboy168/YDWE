16463bff3ba0b98d6428b8473671cbd670d8e4e0
actboy168 <actboy168@gmail.com>
2016-11-23 19:48:26 +0800

    去掉luabind的依赖

--- Development/Plugin/Lua/i18n/luaopen_i18n.cpp
+++ Development/Plugin/Lua/i18n/luaopen_i18n.cpp
@@ -1,21 +1,39 @@
 #pragma warning(push, 3)
 #include <lua.hpp>
-#include <luabind/luabind.hpp>
-#include <base/lua/luabind.h>
 #pragma warning(pop)
 #include <base/i18n/libintl.h>	
 
-int luaopen_i18n(lua_State *pState)
+namespace i18n {
+
+	int gettext(lua_State* L)
+	{
+		auto str = base::i18n::gettext(luaL_checkstring(L, 1));
+		lua_pushlstring(L, str.data(), str.size());
+		return 1;
+	}
+
+	int textdomain(lua_State* L)
+	{
+		base::i18n::textdomain(luaL_checkstring(L, 1));
+		return 0;
+	}
+
+	int bindtextdomain(lua_State* L)
+	{
+		base::i18n::bindtextdomain(luaL_checkstring(L, 1), *(fs::path*)luaL_checkudata(L, 2, "filesystem"));
+		return 0;
+	}
+}
+
+int luaopen_i18n(lua_State* L)
 {
-	using namespace luabind;
-
-	module(pState, "i18n")
-	[
-		def("gettext",        &base::i18n::gettext),
-		def("dgettext",       &base::i18n::dgettext),
-		def("textdomain",     &base::i18n::textdomain),
-		def("bindtextdomain", &base::i18n::bindtextdomain)
-	];
-
+	luaL_Reg l[] = {
+		{ "gettext", i18n::gettext },
+		{ "textdomain", i18n::textdomain },
+		{ "bindtextdomain", i18n::bindtextdomain },
+		{ NULL, NULL },
+	};
+	luaL_newlib(L, l);
+	lua_setglobal(L, "i18n");
 	return 0;
 }
