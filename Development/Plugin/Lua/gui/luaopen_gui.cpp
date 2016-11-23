555c2ebbcbf085c1bebb9d9cae67a6bb907a6c37
actboy168 <actboy168@gmail.com>
2016-11-23 17:01:54 +0800

    去掉对luabind的依赖

--- Development/Plugin/Lua/gui/luaopen_gui.cpp
+++ Development/Plugin/Lua/gui/luaopen_gui.cpp
@@ -1,122 +1,124 @@
 #pragma warning(push, 3)
 #include <lua.hpp>
-#include <luabind/luabind.hpp>
-#include <base/lua/luabind.h>
 #pragma warning(pop)
 #include <base/exception/windows_exception.h>  
 #include <base/util/unicode.h>
+#include <tuple>
 #include <Windows.h>
 #include <cstdint>
 #include "resource.h"
 
 extern HMODULE self_module;
 
-namespace NLuaAPI { namespace NGUI {
+std::wstring luaL_checkwstring(lua_State* L, int index)
+{
+	size_t len = 0;
+	const char* str = luaL_checklstring(L, index, &len);
+	return base::u2w(std::string_view(str, len), base::conv_method::replace | '?');
+}
 
-	typedef boost::tuple<lua_State *, std::wstring, std::wstring,
-		std::wstring, std::wstring, std::wstring> TInputDialogInitInfo;
+void lua_pushwstring(lua_State* L, const std::wstring& str)
+{
+	std::string ustr = base::w2u(str, base::conv_method::replace | '?');
+	lua_pushlstring(L, ustr.data(), ustr.size());
+}
 
-	struct SInputInfo
+typedef std::tuple<lua_State *, std::wstring, std::wstring, std::wstring, std::wstring, std::wstring> TInputDialogInitInfo;
+
+struct SInputInfo
+{
+	bool isOkClicked;
+	wchar_t text[128 * 1024];
+};
+
+static INT_PTR CALLBACK PromptForInputDialogProc(HWND dialogHandle, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	BOOL result = TRUE;
+
+	// Initialization
+	if (message == WM_INITDIALOG)
 	{
-		bool isOkClicked;
-		wchar_t text[128 * 1024];
-	};
+		// Text initialization
+		TInputDialogInitInfo* pInfo = reinterpret_cast<TInputDialogInitInfo *>(lParam);
 
-	static INT_PTR CALLBACK PromptForInputDialogProc(HWND dialogHandle, UINT message, WPARAM wParam, LPARAM lParam)
+		SetWindowTextW(dialogHandle, std::get<1>(*pInfo).c_str());
+		SetDlgItemTextW(dialogHandle, IDC_PROMPT, std::get<2>(*pInfo).c_str());
+		SetDlgItemTextW(dialogHandle, IDC_EDIT_CONTENT, std::get<3>(*pInfo).c_str());
+		SetDlgItemTextW(dialogHandle, IDOK, std::get<4>(*pInfo).c_str());
+		SetDlgItemTextW(dialogHandle, IDCANCEL, std::get<5>(*pInfo).c_str());
+	}
+	// Clicked button
+	else if (message == WM_COMMAND)
 	{
-		BOOL result = TRUE;
-
-		// Initialization
-		if (message == WM_INITDIALOG)
-		{
-			// Text initialization
-			TInputDialogInitInfo *pInfo = reinterpret_cast<TInputDialogInitInfo *>(lParam);
-
-			SetWindowTextW(dialogHandle, pInfo->get<1>().c_str());
-			SetDlgItemTextW(dialogHandle, IDC_PROMPT, pInfo->get<2>().c_str());
-			SetDlgItemTextW(dialogHandle, IDC_EDIT_CONTENT, pInfo->get<3>().c_str());
-			SetDlgItemTextW(dialogHandle, IDOK, pInfo->get<4>().c_str());
-			SetDlgItemTextW(dialogHandle, IDCANCEL, pInfo->get<5>().c_str());
-		}
-		// Clicked button
-		else if (message == WM_COMMAND)
-		{
-			// ID that triggered
-			UINT id = LOWORD(wParam);
-			// Clicked "OK" or "Cancel"
-			if (id == IDOK || id == IDCANCEL)
-			{
-				// TODO: Duplicate code.
-				SInputInfo *pInfo = reinterpret_cast<SInputInfo *>(malloc(sizeof(SInputInfo)));
-				if (pInfo)
-				{
-					pInfo->isOkClicked = (id == IDOK);
-					GetDlgItemTextW(dialogHandle, IDC_EDIT_CONTENT, pInfo->text, sizeof(pInfo->text) / sizeof(pInfo->text[0]));
-				}
-
-				EndDialog(dialogHandle, reinterpret_cast<INT_PTR>(pInfo));
-			}
-		}
-		// Clicked close
-		else if (message == WM_CLOSE)
+		// ID that triggered
+		UINT id = LOWORD(wParam);
+		// Clicked "OK" or "Cancel"
+		if (id == IDOK || id == IDCANCEL)
 		{
 			// TODO: Duplicate code.
 			SInputInfo *pInfo = reinterpret_cast<SInputInfo *>(malloc(sizeof(SInputInfo)));
 			if (pInfo)
 			{
-				pInfo->isOkClicked = false;
+				pInfo->isOkClicked = (id == IDOK);
 				GetDlgItemTextW(dialogHandle, IDC_EDIT_CONTENT, pInfo->text, sizeof(pInfo->text) / sizeof(pInfo->text[0]));
 			}
 
 			EndDialog(dialogHandle, reinterpret_cast<INT_PTR>(pInfo));
 		}
-		// Messages that not interested
-		else
-		{
-			result = FALSE;
-		}
-
-		return result;
 	}
-
-	static void LuaGuiPromptForInput(lua_State *pState, void *parentWindowHandle, const std::wstring &caption, const std::wstring &prompt, const std::wstring &initialText, const std::wstring &okCaption, const std::wstring &cancelCaption)
+	// Clicked close
+	else if (message == WM_CLOSE)
 	{
-		TInputDialogInitInfo dialogInitInfo(
-			pState, caption, prompt, initialText, okCaption, cancelCaption
-			);
-
-		SInputInfo *pInputInfo = reinterpret_cast<SInputInfo *>(
-			::DialogBoxParamW(self_module, MAKEINTRESOURCEW(IDD_DIALOG_INPUT),
-			reinterpret_cast<HWND>(parentWindowHandle), PromptForInputDialogProc,
-			reinterpret_cast<LPARAM>(&dialogInitInfo)
-			)
-			);
-
-		if ((intptr_t)pInputInfo == -1)
+		// TODO: Duplicate code.
+		SInputInfo *pInfo = reinterpret_cast<SInputInfo *>(malloc(sizeof(SInputInfo)));
+		if (pInfo)
 		{
-			base::windows_exception e;
-			luaL_error(pState, e.what());
+			pInfo->isOkClicked = false;
+			GetDlgItemTextW(dialogHandle, IDC_EDIT_CONTENT, pInfo->text, sizeof(pInfo->text) / sizeof(pInfo->text[0]));
 		}
-		else
-		{
-			luabind::object(pState, (bool)pInputInfo->isOkClicked).push(pState);
-			luabind::object(pState, std::wstring(pInputInfo->text)).push(pState);
-			free(pInputInfo);
-		}
+
+		EndDialog(dialogHandle, reinterpret_cast<INT_PTR>(pInfo));
 	}
+	// Messages that not interested
+	else
+	{
+		result = FALSE;
+	}
+
+	return result;
 }
 
-}
-
-int luaopen_gui(lua_State *pState)
+static int LuaGuiPromptForInput(lua_State* L)
 {
-	using namespace luabind;
+	HWND parentWindowHandle = (HWND)luaL_checkinteger(L, 1);
+	std::wstring caption = luaL_checkwstring(L, 2);
+	std::wstring prompt = luaL_checkwstring(L, 3);
+	std::wstring initialText = luaL_checkwstring(L, 4);
+	std::wstring okCaption = luaL_checkwstring(L, 5);
+	std::wstring cancelCaption = luaL_checkwstring(L, 6);
 
-	module(pState, "gui")
-	[
-		def("prompt_for_input",   &NLuaAPI::NGUI::LuaGuiPromptForInput)
-	];
+	TInputDialogInitInfo dialogInitInfo(L, caption, prompt, initialText, okCaption, cancelCaption);
 
-	lua_getglobal(pState, "gui");
+	SInputInfo *pInputInfo = reinterpret_cast<SInputInfo *>(::DialogBoxParamW(self_module, MAKEINTRESOURCEW(IDD_DIALOG_INPUT), parentWindowHandle, PromptForInputDialogProc, reinterpret_cast<LPARAM>(&dialogInitInfo)));
+
+	if ((intptr_t)pInputInfo == -1)
+	{
+		base::windows_exception e;
+		return luaL_error(L, e.what());
+	}
+	lua_pushboolean(L, pInputInfo->isOkClicked);
+	lua_pushwstring(L, std::wstring(pInputInfo->text));
+	free(pInputInfo);
+	return 2;
+}
+
+int luaopen_gui(lua_State* L)
+{
+	luaL_Reg l[] = {
+		{ "prompt_for_input", LuaGuiPromptForInput },
+		{ NULL, NULL },
+	};
+	luaL_newlib(L, l);
+	lua_setglobal(L, "gui");
 	return 0;
 }
