1db8ce8c8c9c54cab712addb7a1de3364ae9eaac
actboy168 <actboy168@gmail.com>
2016-12-05 22:51:49 +0800

    移除了grimext

--- Development/Component/grimext/samples/fetest.lua
+++
@@ -1,8 +0,0 @@
-exportgroup(triggers,"export")
-exportgroup(objects,"export\\")
-exportgroup(constants,"unittests\\output\\export")
-exportgroup(environment,"unittests\\output\\export\\")
-exportgroup(imports,"export")
-exportfile("war3map.j","export")
-exportfile("import\\import3.txt","export")
-exportfile("war3mapImported\\import1.txt","export",false)
