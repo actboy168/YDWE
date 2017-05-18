04f551be52b45a2fed5f900c8b3daa64fe19016e
actboy168 <actboy168@gmail.com>
2017-05-18 14:54:51 +0800

    支持指定调试器

--- Development/Plugin/Warcraft3/yd_lua_engine/lua_engine/libs_debugger.cpp
+++ Development/Plugin/Warcraft3/yd_lua_engine/lua_engine/libs_debugger.cpp
@@ -1,12 +1,22 @@
 #include <Windows.h>
 #include <lua.hpp>
+#include <base/warcraft3/command_line.h>
 
 namespace base { namespace warcraft3 { namespace lua_engine { namespace debugger {
 	int open(lua_State* L)
 	{
-		HMODULE m = LoadLibraryW(L"debugger.dll");
+		HMODULE m = GetModuleHandleW(L"debugger.dll");
 		if (!m) {
-			return 0;
+			base::warcraft3::command_line cmd;
+			if (cmd.has(L"debugger")) {
+				m = LoadLibraryW(cmd[L"debugger"].c_str());
+			}
+			else {
+				m = LoadLibraryW(L"debugger.dll");
+			}
+			if (!m) {
+				return 0;
+			}
 		}
 		lua_CFunction openf = (lua_CFunction)GetProcAddress(m, "luaopen_debugger");
 		if (!openf) {
