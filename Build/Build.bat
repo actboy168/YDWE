45735219cf2c3276c7e8cbbac56366fd2bc21a12
actboy168 <actboy168@gmail.com>
2017-09-26 13:02:55 +0800

    不要引号

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,5 +1,5 @@
 @echo OFF
-echo "%~dp0ok.lock"
-if exist "%~dp0ok.lock" (set errorlevel=0) else (set errorlevel=1)
+echo %~dp0ok.lock
+if exist %~dp0ok.lock (set errorlevel=0) else (set errorlevel=1)
 echo errorlevel: %errorlevel%
 exit /b %errorlevel%
