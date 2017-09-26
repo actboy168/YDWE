d25309b35fe89b713aa72e2df2dcc231eb7381df
actboy168 <actboy168@gmail.com>
2017-09-26 12:57:52 +0800

    测试下

--- Build/Build_Release.bat
+++ Build/Build_Release.bat
@@ -1,2 +1,5 @@
-call "%~dp0Build.bat" Release
-@PAUSE
+@echo OFF
+echo "%~dp0ok.lock"
+if exist "%~dp0ok.lock" (set errorlevel=0) else (set errorlevel=1)
+echo errorlevel: %errorlevel%
+exit /b %errorlevel%
