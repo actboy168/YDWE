eadd8559bb6092ebb843de8451e0e2a85aea2fbc
actboy168 <actboy168@gmail.com>
2017-09-26 13:13:19 +0800

    打开echo

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,4 +1,3 @@
-@echo OFF
 CD lua\luabuild\bin
 "%~dp0lua\luabuild\bin\lua.exe" "%~dp0lua\make.lua" "%~dp0lua" %1 "%~dp0build.log" %2
 CD ..\..\..
--- Build/test.bat
+++ Build/test.bat
@@ -1,4 +1,3 @@
-@echo OFF
 echo %~dp0ok.lock
 if exist %~dp0ok.lock (set errorlevel=0) else (set errorlevel=1)
 echo errorlevel: %errorlevel%
