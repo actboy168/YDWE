034070280f955dc96b9b7dd289b9ce33b15b91d7
actboy168 <actboy168@gmail.com>
2017-09-26 13:04:57 +0800

    test.bat

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,5 +1,4 @@
 @echo OFF
-echo %~dp0ok.lock
-if exist %~dp0ok.lock (set errorlevel=0) else (set errorlevel=1)
-echo errorlevel: %errorlevel%
-exit /b %errorlevel%
+CD lua\luabuild\bin
+"%~dp0lua\luabuild\bin\lua.exe" "%~dp0lua\make.lua" "%~dp0lua" %1 "%~dp0build.log" %2
+CD ..\..\..
--- Build/test.bat
+++ Build/test.bat
@@ -1,4 +1,5 @@
-@echo off
-if exist "%~dp0ok.lock" (set errorlevel=0) else (set errorlevel=1)
+@echo OFF
+echo %~dp0ok.lock
+if exist %~dp0ok.lock (set errorlevel=0) else (set errorlevel=1)
 echo errorlevel: %errorlevel%
 exit /b %errorlevel%
