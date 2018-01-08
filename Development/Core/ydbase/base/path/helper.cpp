acfa3816a730925e1d7c4389a816244b82cd7397
actboy168 <actboy168@gmail.com>
2018-01-08 17:03:44 +0800

    增加一个计算ydwe路径的函数

--- Development/Core/ydbase/base/path/helper.cpp
+++ Development/Core/ydbase/base/path/helper.cpp
@@ -1,4 +1,5 @@
 #include <base/path/helper.h>
+#include <base/path/service.h>
 #include <base/util/foreach.h>
 #include <deque>
 
@@ -37,5 +38,18 @@ namespace base { namespace path {
 			++l; ++r; 
 		}
 		return *l == *r;
+	}
+
+	fs::path ydwe(bool support_dev)
+	{
+		fs::path ydwe = get(DIR_MODULE);
+		ydwe.remove_filename().remove_filename();
+		if (support_dev) {
+			fs::path ydwedev = ydwe.parent_path().remove_filename().remove_filename();
+			if (fs::exists(ydwedev / "build.root")) {
+				ydwe = ydwedev / L"Component";
+			}
+		}
+		return ydwe;
 	}
 }}
--- Development/Core/ydbase/base/path/helper.h
+++ Development/Core/ydbase/base/path/helper.h
@@ -6,4 +6,5 @@
 namespace base { namespace path {
 	_BASE_API bool equal(fs::path const& lhs, fs::path const& rhs);
 	_BASE_API fs::path normalize(const fs::path& p);
+	_BASE_API fs::path ydwe(bool support_dev);
 }}
