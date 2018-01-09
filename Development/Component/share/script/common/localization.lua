390585bffd2ba14ff1ecde631d2af2240fee9dba
actboy168 <actboy168@gmail.com>
2018-01-09 14:26:10 +0800

    已经不需要了

--- Development/Component/share/script/common/localization.lua
+++ Development/Component/share/script/common/localization.lua
@@ -6,10 +6,5 @@ function _(str)
 	return i18n.get_text(str)
 end
 
-local ydwe = fs.ydwe_path():parent_path():remove_filename():remove_filename() / "Component"
-if not fs.exists(ydwe) then
-	ydwe = fs.ydwe_path()
-end
-
 i18n.initialize(fs.ydwe_devpath() / "share" / "locale")
 i18n.set_domain("script")
