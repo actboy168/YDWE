6db4be6037bd8ced5268453ea99b0acd8bccab81
最萌小汐 <76196625@qq.com>
2018-01-17 10:11:04 +0800

    mpq路径计算不要依赖w3x2lni

--- Development/Component/share/script/ydwe/mpqloader.lua
+++ Development/Component/share/script/ydwe/mpqloader.lua
@@ -1,5 +1,46 @@
 local event = require 'ev'
-local mpq_path = require 'w3x2lni.mpq_path'
+
+local function mpq_path()
+    local mt = {}
+    local paths = {'\\'}
+    local mpqs = {}
+    local function update()
+        paths = {'\\'}
+        for i = #mpqs, 1, -1 do
+            local path = mpqs[i]
+            local max = #paths
+            table.insert(paths, '\\' .. path .. '\\')
+            for i = 2, max do
+                table.insert(paths, '\\' .. path .. paths[i])
+            end
+        end
+    end
+    function mt:open(path)
+        table.insert(mpqs, path)
+        update()
+    end
+    function mt:close(path)
+        for i, mpq in ipairs(mpqs) do
+            if mpq == path then
+                table.remove(mpqs, i)
+                update()
+                return
+            end
+        end
+    end
+    function mt:each_path(callback)
+        for i = #paths, 1, -1 do
+            local res = callback(paths[i])
+            if res then
+                return res
+            end
+        end
+    end
+    function mt:first_path()
+        return paths[#paths]
+    end
+    return mt
+end
 
 local mt = {}
 
