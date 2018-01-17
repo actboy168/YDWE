b8aa7b659ed08d5a057f763f3d375871465c53ab
最萌小汐 <76196625@qq.com>
2018-01-17 10:09:27 +0800

    读写文件添加默认值

--- Development/Component/plugin/w3x2lni/init.lua
+++ Development/Component/plugin/w3x2lni/init.lua
@@ -218,6 +218,24 @@ local function new_path()
     return mt
 end
 
+function mt:map_load(filename)
+    return nil
+end
+
+function mt:map_save(filename, buf)
+end
+
+function mt:map_remove(filename)
+end
+
+function mt:mpq_load(filename)
+    return nil
+end
+
+function mt:prebuilt_load(filename)
+    return nil
+end
+
 mt.config = {}
 function mt:set_config(config)
     self.config = config
