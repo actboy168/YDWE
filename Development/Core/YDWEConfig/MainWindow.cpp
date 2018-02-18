e4e64143e6178f1af7a6d7e8699ada6582790c4a
actboy168 <actboy168@gmail.com>
2018-02-18 22:04:32 +0800

    移除显示物体属性内部ID

--- Development/Core/YDWEConfig/MainWindow.cpp
+++ Development/Core/YDWEConfig/MainWindow.cpp
@@ -92,7 +92,6 @@ namespace
 		("EnableYDTrigger",                         Attribute("ThirdPartyPlugin"))
 		("EnableManualNewId",                       Attribute("FeatureToggle"))
 		("EnableTriggerCopyEncodingAutoConversion", Attribute("FeatureToggle"))
-		("EnableShowInternalAttributeId",           Attribute("FeatureToggle"))
 		("FontEnable",                              Attribute("Font"))
 		("FontName",                                Attribute("Font", Attribute::e_ComboBox))
 		("FontSize",                                Attribute("Font", Attribute::e_ComboBox))
@@ -209,7 +208,6 @@ void CMainWindow::ResetConfig(base::ini::table& table)
 	table["ThirdPartyPlugin"]["EnableYDTrigger"] = "1";
 	table["FeatureToggle"]["EnableManualNewId"] = "0";
 	table["FeatureToggle"]["EnableTriggerCopyEncodingAutoConversion"] = "1";
-	table["FeatureToggle"]["EnableShowInternalAttributeId"] = "0";
 	table["Font"]["FontEnable"] = "0";
 	table["Font"]["FontName"] = "";
 	table["Font"]["FontSize"] = "12";
