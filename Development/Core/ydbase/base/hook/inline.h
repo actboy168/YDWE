8ba4130e3dab4e8d3f562750ec131a4a21f83dfa
actboy168 <actboy168@gmail.com>
2018-02-19 13:34:35 +0800

    关闭detours

--- Development/Core/ydbase/base/hook/inline.h
+++ Development/Core/ydbase/base/hook/inline.h
@@ -3,11 +3,9 @@
 #include <base/config.h>
 #include <cstdint>
 
-namespace base { namespace hook {  
-#if !defined(DISABLE_DETOURS)
+namespace base { namespace hook {
 	typedef void* hook_t;
 	_BASE_API bool install(uintptr_t* pointer_ptr, uintptr_t detour, hook_t* ph = nullptr);
 	_BASE_API bool uninstall(hook_t* ph);
-#endif
 	_BASE_API uintptr_t replace_pointer(uintptr_t address, uintptr_t new_value);
 }}
