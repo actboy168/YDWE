6f53bde0ea424644c8b06d6103b0612bca5f525c
actboy168 <actboy168@gmail.com>
2018-02-23 17:03:34 +0800

    这个日志等级错了

--- Development/Component/share/script/common/localization.lua
+++ Development/Component/share/script/common/localization.lua
@@ -3,7 +3,7 @@ local i18n = require "i18n"
 i18n.initialize(fs.ydwe_devpath() / "share" / "locale")
 i18n.set_domain("script")
 
-log.error('[i18n]current language', i18n.get_language())
+log.info('[i18n]current language', i18n.get_language())
 
 LNG = setmetatable({}, { __index = function (_, str)
     local res = i18n.get_text(str)
