d1d2ee919bf262055a40b72f5606d74bf067b7f6
actboy168 <actboy168@gmail.com>
2018-03-24 18:05:52 +0800

    移除重复的代码

--- Development/Component/share/script/ydwe/w3x2lni_in_sandbox.lua
+++ Development/Component/share/script/ydwe/w3x2lni_in_sandbox.lua
@@ -1,6 +1,6 @@
-local sandbox = require 'sandbox'
-local root = fs.ydwe_devpath() / 'plugin' / 'w3x2lni' / 'core'
-return sandbox(root:string() .. '\\', io.__open, {
+local root = fs.ydwe_devpath() / 'plugin' / 'w3x2lni'
+local sandbox = loadfile((root / 'tool' / 'sandbox.lua'):string())()
+return sandbox(root:string() .. '\\core\\', io.__open, {
     ['w3xparser'] = require 'w3xparser',
     ['lni']       = require 'lni',
     ['lpeg']      = require 'lpeg',
