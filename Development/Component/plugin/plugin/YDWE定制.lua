af9f7cf6ba4d8029e5f74253d2ed4042a1dcb847
最萌小汐 <sumneko@hotmail.com>
2018-04-25 17:57:52 +0800

    可以优化后测试了

---
+++ Development/Component/plugin/plugin/YDWE定制.lua
@@ -0,0 +1,52 @@
+local mt = {}
+
+mt.info = {
+    name = 'YDWE定制',
+    version = 1.0,
+    author = '最萌小汐',
+    description = '修改mpq与prebuilt的读写路径。',
+}
+
+function mt:on_convert(w2l)
+    require 'filesystem'
+    local lang = require 'ffi.language' ()
+    local mpq_path = require 'core.mpq_path' ()
+
+    mpq_path:open(lang)
+
+    local updated
+    local function update_version()
+        if updated then
+            return
+        end
+        updated = true
+        if w2l.config.version == 'Melee' then
+            mpq_path:open 'Melee_V1'
+        else
+            mpq_path:open 'Custom_V1'
+        end
+    end
+    
+    local root = fs.ydwe(true)
+    local ydpath = fs.ydwe(false)
+    local war3_path = root / 'share' / 'mpq'
+    function w2l:mpq_load(filename)
+        return mpq_path:each_path(function (path)
+            return io.load(war3_path / path / filename)
+        end)
+    end
+
+    local prebuilt_path = ydpath / 'script' / 'ydwe' / 'prebuilt'
+    function w2l:prebuilt_load(filename)
+        return mpq_path:each_path(function (path)
+            return io.load(prebuilt_path / path / filename)
+        end)
+    end
+    function w2l:prebuilt_save(filename, buf)
+        local path = prebuilt_path / mpq_path:first_path() / filename
+        fs.create_directories(path:parent_path())
+        io.save(path, buf)
+	end
+end
+
+return mt
