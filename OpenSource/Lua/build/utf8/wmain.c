64e2352c2989636d8976e1323ee2a774b3ea735e
actboy168 <actboy168@gmail.com>
2018-05-08 20:34:07 +0800

    修正debug版的错误

--- OpenSource/Lua/build/utf8/wmain.c
+++ OpenSource/Lua/build/utf8/wmain.c
@@ -3,10 +3,11 @@
 #include "unicode.h"
 
 int wmain(int argc, wchar_t **wargv) {
-	char **argv = calloc(argc, sizeof(char*));
+	char **argv = calloc(argc + 1, sizeof(char*));
 	for (int i = 0; i < argc; ++i) {
 		argv[i] = w2u(wargv[i]);
 	}
+	argv[argc] = 0;
 
 	int ret = main_utf8(argc, argv);
 
