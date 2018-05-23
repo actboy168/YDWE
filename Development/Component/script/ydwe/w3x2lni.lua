d196a81d7bbbcb74c3c84b6245ec549794110a14
actboy168 <actboy168@gmail.com>
2018-05-23 23:12:18 +0800

    修正一个错误

--- Development/Component/script/ydwe/w3x2lni.lua
+++ Development/Component/script/ydwe/w3x2lni.lua
@@ -1,5 +1,5 @@
 local root = fs.ydwe_devpath() / 'plugin' / 'w3x2lni' / 'script' / 'core'
-local sandbox = loadfile((root / 'backend' / 'sandbox.lua'):string())()
+local sandbox = loadfile((fs.ydwe_devpath() / 'plugin' / 'w3x2lni' / 'script' / 'backend' / 'sandbox.lua'):string())()
 return sandbox(root:string() .. '\\', io.__open, {
     ['w3xparser'] = require 'w3xparser',
     ['lni']       = require 'lni',
