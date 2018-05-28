42e6452a6b56e806d298dac6d1411c6e8ef1b377
最萌小汐 <sumneko@hotmail.com>
2018-05-28 11:27:38 +0800

    修正读ui文件的一个BUG

--- Development/Component/script/ydwe/data_load.lua
+++ Development/Component/script/ydwe/data_load.lua
@@ -3,7 +3,7 @@ local root = fs.ydwe_devpath()
 local w2l_path = root / 'plugin' / 'w3x2lni'
 
 return function (w2l, filename)
-    if filename:sub(1, 6) == 'ui/' and w2l.setting.data_ui == '${YDWE}' then
+    if filename:sub(1, 3) == 'ui/' then
         return io.load(root / filename)
     end
     return io.load(w2l_path / 'data' / w2l.setting.data / filename)
