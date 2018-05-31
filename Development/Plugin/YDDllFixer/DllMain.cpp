946ad755f9ad9c16ae03b41158004f2d67c8feb1
actboy168 <actboy168@gmail.com>
2018-05-31 17:15:57 +0800

    去掉对fs库的依赖

--- Development/Plugin/YDDllFixer/DllMain.cpp
+++ Development/Plugin/YDDllFixer/DllMain.cpp
@@ -1,22 +1,89 @@
 #include <deque>
-#include <filesystem>
-#include <utility>
 #include <windows.h>
 #include <Shlwapi.h>
-#include <base/filesystem.h>
 
-static fs::path GetModuleDirectory(HMODULE hModule)
-{
-	fs::path result(L".");
-	wchar_t buffer[MAX_PATH];
+struct strview {
+	const wchar_t* buf;
+	size_t len;
+	strview(const wchar_t* str);
+	strview(const wchar_t* str, size_t len);
+};
 
-	if (::GetModuleFileNameW(hModule, buffer, sizeof(buffer) / sizeof(buffer[0])))
-	{
-		result = buffer;
-		result = result.parent_path(); // 获取上级路径
+template <size_t N>
+struct strbuilder {
+	wchar_t buf[N];
+	size_t  len = 0;
+	void append(const wchar_t* str, size_t n) {
+		if (len + n >= sizeof buf / sizeof buf[0]) {
+			return;
+		}
+		wcsncpy(buf + len, str, n);
+		len += n;
 	}
+	template <class T, size_t n>
+	void operator +=(T(&str)[n]) {
+		append(str, n - 1);
+	}
+	template <size_t n>
+	void operator +=(const strbuilder<n>& str) {
+		append(str.buf, str.len);
+	}
+	void operator +=(const strview& str) {
+		append(str.buf, str.len);
+	}
+	wchar_t* string() {
+		buf[len] = L'\0';
+		return buf;
+	}
+};
 
-	return std::move(result);
+struct path : public strbuilder<MAX_PATH> {
+	path(HMODULE m);
+	template <class T, size_t n>
+	path& operator /=(T(&str)[n]) {
+		*this += L"\\";
+		*this += str;
+		return *this;
+	}
+	path& operator /=(const wchar_t* str) {
+		*this += L"\\";
+		*this += str;
+		return *this;
+	}
+};
+
+template <class T, size_t n>
+path operator /(path& self, T(&str)[n]) {
+	path res = self;
+	res += L"\\";
+	res += str;
+	return res;
+}
+
+path operator /(path& self, const wchar_t* str) {
+	path res = self;
+	res += L"\\";
+	res += str;
+	return res;
+}
+
+strview::strview(const wchar_t* str)
+	: buf(str)
+	, len(wcslen(str))
+{ }
+
+strview::strview(const wchar_t* str, size_t len)
+	: buf(str)
+	, len(len)
+{ }
+
+path::path(HMODULE m) {
+	::GetModuleFileNameW(m, buf, sizeof buf / sizeof buf[0]);
+	::PathRemoveBlanksW(buf);
+	::PathUnquoteSpacesW(buf);
+	::PathRemoveBackslashW(buf);
+	::PathRemoveFileSpecW(buf);
+	len = wcslen(buf);
 }
 
 const wchar_t* szDllList[] = {
@@ -35,14 +102,13 @@ std::deque<HMODULE> hDllArray;
 
 void PreloadDll(HMODULE module)
 {
-	fs::path ydweDirectory = GetModuleDirectory(module);
-	fs::path binPath = ydweDirectory.parent_path() / L"bin";
+	path binPath = path(module);
 	wchar_t buffer[MAX_PATH];
 	::GetCurrentDirectoryW(sizeof(buffer) / sizeof(buffer[0]), buffer);
-	::SetCurrentDirectoryW(binPath.wstring().c_str());
+	::SetCurrentDirectoryW(binPath.string());
 	for (const wchar_t *szDllName : szDllList)
 	{
-		hDllArray.push_front(::LoadLibraryW((binPath / szDllName).wstring().c_str()));
+		hDllArray.push_front(::LoadLibraryW((binPath / szDllName).string()));
 	}
 	::SetCurrentDirectoryW(buffer);
 }
