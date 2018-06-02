b99d1c504f7482750f8d6034f21ad482c29511bf
actboy168 <actboy168@gmail.com>
2018-06-02 20:51:20 +0800

    整理代码

--- Development/Component/script/ydwe/ydwe_on_save.lua
+++ Development/Component/script/ydwe/ydwe_on_save.lua
@@ -1,22 +1,42 @@
 local compiler = require "compile.compiler"
 local lnisaver = require 'w3x2lni.lnisaver'
 
--- 确定应当把地图保存为适合老版本（< 1.24）还是新版本（>= 1.24）的
--- 对于保存为双份，按照当前魔兽版本处理
--- save_type - 保存类型，0到3的取值，意义同设置程序，从上到下依次为0到3
--- runtime_version - 魔兽版本
--- 返回值: 数，如果保存的目标版本和当前版本一致，返回当前版本，否则视情况返回1.20或者1.24（和当前版本不同）
-local function determine_map_version(save_type, runtime_version)
-	-- 固定旧版本
+local function make_option(config, war3ver)
+	local option = {}
+	-- 是否启用JassHelper
+	option.enable_jasshelper = config.ScriptCompiler.EnableJassHelper ~= "0"
+	-- 是否使用脚本模式编译
+	option.enable_jasshelper_scriptonly = config.ScriptCompiler.EnableJassHelperScriptOnly == "1"
+	-- 是否是调试模式
+	option.enable_jasshelper_debug = config.ScriptCompiler.EnableJassHelperDebug == "1"
+	-- 是否优化地图
+	option.enable_jasshelper_optimization = config.ScriptCompiler.EnableJassHelperOptimization ~= "0"
+	-- 是否启用cJass
+	option.enable_cjass = config.ScriptCompiler.EnableCJass == "1"
+	-- 是否启用YDTrigger
+	option.enable_yd_trigger = plugin.loaders['YDTrigger'] ~= nil
+	-- pjass的版本
+	option.pjass = config.PJass.Option
+	-- 代码注入选项
+	-- cjass和脚本注入不能共存
+	if option.enable_cjass then
+		option.script_injection = false
+	else
+		option.script_injection = tonumber(config.ScriptInjection.Option)
+	end
+	-- 目标魔兽版本
+	local save_type = tonumber(config.MapSave.Option)
 	if save_type == 1 then
-		return 20
-	-- 固定新版本
+		-- 固定旧版本
+		option.runtime_version = 20
 	elseif save_type == 2 then
-		return 24
+		-- 固定新版本
+		option.runtime_version = 24
 	else
 		-- 按照当前版本或者双份
-		return runtime_version:is_new() and 24 or 20
+		option.runtime_version = war3ver
 	end
+	return option
 end
 
 -- 本函数当保存地图时调用
@@ -33,38 +53,8 @@ function event.EVENT_SAVE_MAP(event_data)
 	local map_path = fs.path(event_data.map_path)
 	log.trace("Saving " .. map_path:string())
 
-	-- 获取保存类型
-	local save_type = tonumber(global_config["MapSave"]["Option"])
-
-	-- 获取保存选项
-	local save_option = {
-		-- 是否启用JassHelper
-		enable_jasshelper = (global_config["ScriptCompiler"]["EnableJassHelper"] ~= "0"),
-		-- 是否使用脚本模式编译
-		enable_jasshelper_scriptonly = (global_config["ScriptCompiler"]["EnableJassHelperScriptOnly"] == "1"),
-		-- 是否是调试模式
-		enable_jasshelper_debug = (global_config["ScriptCompiler"]["EnableJassHelperDebug"] == "1"),
-		-- 是否优化地图
-		enable_jasshelper_optimization = (global_config["ScriptCompiler"]["EnableJassHelperOptimization"] ~= "0"),
-		-- 是否启用cJass
-		enable_cjass = (global_config["ScriptCompiler"]["EnableCJass"] == "1"),
-		-- 目标魔兽版本
-		runtime_version = determine_map_version(save_type, war3_version),
-		-- 代码注入选项
-		script_injection = tonumber(global_config["ScriptInjection"]["Option"]),
-		-- 是否启用YDTrigger
-		enable_yd_trigger = plugin.loaders['YDTrigger'] ~= nil,
-		-- pjass的版本
-		pjass = global_config["PJass"]["Option"],
-	}
-
-	-- cjass和脚本注入不能共存
-	if save_option.enable_cjass then
-		save_option.script_injection = false
-	end
-
 	-- 编译地图
-	local result = compiler:compile(map_path, save_option)
+	local result = compiler:compile(map_path, make_option(global_config, war3_version:is_new() and 24 or 20))
 	if result then
 		-- 转换成Lni地图
 		result = lnisaver(map_path)
