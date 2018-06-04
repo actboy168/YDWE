87ce41c35d31177eb9208edeb0e1269697fb48c0
最萌小汐 <sumneko@hotmail.com>
2018-06-04 14:17:06 +0800

    更新w3x2lni

--- Development/Component/plugin/w3x2lni/script/backend/ydwe_path.lua
+++ Development/Component/plugin/w3x2lni/script/backend/ydwe_path.lua
@@ -1,5 +1,5 @@
 return function()
-    require 'registry'
+    local registry = require 'registry'
     local commands = registry.open [[HKEY_CURRENT_USER\SOFTWARE\Classes\YDWEMap\shell\run_war3\command]]
     if not commands then
         return
