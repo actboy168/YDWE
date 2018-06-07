99baf0885568d5085b7e712c9cd9c3f093d407c4
actboy168 <actboy168@gmail.com>
2018-06-07 17:36:14 +0800

    预处理放回原来的地方

--- Development/Component/script/ydwe/w3x2lni/data_load.lua
+++ Development/Component/script/ydwe/w3x2lni/data_load.lua
@@ -7,6 +7,9 @@ return function (w2l, filename)
     if filename:sub(1, 3) == 'ui/' then
         return io.load(root / filename)
     end
+    if filename:sub(1, 14) == 'data/prebuilt/' then
+        return io.load(w2l_path / 'data' / w2l.setting.data / filename:sub(6))
+    end
     if filename:sub(1, 5) == 'data/' then
         return io.load(root / 'share' / current_language / filename:sub(6))
     end
