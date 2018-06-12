5abd50a2c5923e3a873adc224173292f0c0f0ca7
actboy168 <actboy168@gmail.com>
2018-06-12 15:30:31 +0800

    同步代码

--- Development/Component/compiler/script/w3x2lni/data_load.lua
+++ Development/Component/compiler/script/w3x2lni/data_load.lua
@@ -7,11 +7,8 @@ return function (w2l, filename)
     if filename:sub(1, 3) == 'ui/' then
         return io.load(ydwePath / filename)
     end
-    if filename:sub(1, 14) == 'data/prebuilt/' then
-        return io.load(w2lPath / 'data' / w2l.setting.data / filename:sub(6))
+    if filename:sub(1, 9) == 'prebuilt/' then
+        return io.load(w2lPath / 'data' / w2l.setting.data / filename)
     end
-    if filename:sub(1, 5) == 'data/' then
-        return io.load(ydwePath / 'share' / currentLanguage / filename:sub(6))
-    end
-    return io.load(w2lPath / filename)
+    return io.load(ydwePath / 'share' / currentLanguage / filename)
 end
