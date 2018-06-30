c2ed4f0ffb4983f0a7ad7ceec74dd447e4b87ebc
actboy168 <actboy168@gmail.com>
2018-06-30 09:50:26 +0800

    移除没用的代码

--- Development/Core/ydbase/base/path/get_path.cpp
+++ Development/Core/ydbase/base/path/get_path.cpp
@@ -16,24 +16,16 @@ extern "C" IMAGE_DOS_HEADER __ImageBase;
 
 namespace base { namespace path {
 
-	fs::path quick_launch_path(bool default_user)
+	fs::path quick_launch_path()
 	{
-		if (default_user)
-		{
-			wchar_t buffer[MAX_PATH];
-			buffer[0] = 0;
-			// http://msdn.microsoft.com/library/windows/desktop/bb762181.aspx
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_APPDATA, reinterpret_cast<HANDLE>(-1), SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		}
-		else
-		{
-			fs::path result = get(DIR_APP_DATA);
-			// http://stackoverflow.com/questions/76080/how-do-you-reliably-get-the-quick-
-			// http://www.microsoft.com/technet/scriptcenter/resources/qanda/sept05/hey0901.mspx
-			result = result / L"Microsoft" / L"Internet Explorer" / L"Quick Launch";
-			return std::move(result);
-		}
+		wchar_t buffer[MAX_PATH];
+		buffer[0] = 0;
+		ENSURE(::SHGetFolderPathW(NULL, CSIDL_APPDATA, NULL, SHGFP_TYPE_CURRENT, buffer));
+		fs::path result(buffer);
+		// http://stackoverflow.com/questions/76080/how-do-you-reliably-get-the-quick-
+		// http://www.microsoft.com/technet/scriptcenter/resources/qanda/sept05/hey0901.mspx
+		result = result / L"Microsoft" / L"Internet Explorer" / L"Quick Launch";
+		return std::move(result);
 	}
 
 	// 
@@ -67,52 +59,6 @@ namespace base { namespace path {
 			throw windows_exception("::GetTempPathW failed.");
 		}
 		return std::move(p);
-	}
-
-	fs::path windows_path()
-	{
-		wchar_t buffer[MAX_PATH];
-		DWORD path_len = ::GetWindowsDirectoryW(buffer, _countof(buffer));
-		if (path_len == 0)
-		{
-			throw windows_exception("::GetWindowsDirectoryW failed.");
-		}
-
-		if (path_len <= _countof(buffer))
-		{
-			return std::move(fs::path(buffer, buffer + path_len));
-		}
-
-		std::dynarray<wchar_t> buf(path_len);
-		path_len = ::GetWindowsDirectoryW(buf.data(), buf.size());
-		if (path_len == 0 || path_len > _countof(buffer))
-		{
-			throw windows_exception("::GetWindowsDirectoryW failed.");
-		}
-		return std::move(fs::path(buf.begin(), buf.end()));
-	}
-
-	fs::path system_path()
-	{
-		wchar_t buffer[MAX_PATH];
-		DWORD path_len = ::GetSystemDirectoryW(buffer, _countof(buffer));
-		if (path_len == 0)
-		{
-			throw windows_exception("::GetSystemDirectoryW failed.");
-		}
-
-		if (path_len <= _countof(buffer))
-		{
-			return std::move(fs::path(buffer, buffer + path_len));
-		}
-
-		std::dynarray<wchar_t> buf(path_len);
-		path_len = ::GetWindowsDirectoryW(buf.data(), buf.size());
-		if (path_len == 0 || path_len > _countof(buffer))
-		{
-			throw windows_exception("::GetSystemDirectoryW failed.");
-		}
-		return std::move(fs::path(buf.begin(), buf.end()));
 	}
 
 	fs::path module(HMODULE module_handle)
@@ -160,56 +106,14 @@ namespace base { namespace path {
 			return std::move(module(reinterpret_cast<HMODULE>(&__ImageBase)));
 		case DIR_TEMP:
 			return std::move(temp_path());
-		case DIR_WINDOWS:
-			return std::move(windows_path());
-		case DIR_SYSTEM:
-			return std::move(system_path());
-		case DIR_PROGRAM_FILES:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_PROGRAM_FILES, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_IE_INTERNET_CACHE:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_INTERNET_CACHE, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_COMMON_START_MENU:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_COMMON_PROGRAMS, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
 		case DIR_START_MENU:
 			ENSURE(::SHGetFolderPathW(NULL, CSIDL_PROGRAMS, NULL, SHGFP_TYPE_CURRENT, buffer));
 			return std::move(fs::path(buffer));
-		case DIR_APP_DATA:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_APPDATA, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_COMMON_APP_DATA:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_COMMON_APPDATA, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_PROFILE:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_LOCAL_APP_DATA:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_LOCAL_APPDATA, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_SOURCE_ROOT: 
-			{
-				fs::path result = get(DIR_EXE);
-				result = result.parent_path();
-				return std::move(result);
-			}
 		case DIR_USER_DESKTOP:
 			ENSURE(::SHGetFolderPathW(NULL, CSIDL_DESKTOPDIRECTORY, NULL, SHGFP_TYPE_CURRENT, buffer));
 			return std::move(fs::path(buffer));
-		case DIR_COMMON_DESKTOP:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_COMMON_DESKTOPDIRECTORY, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_PERSONAL:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
-		case DIR_MYPICTURES:
-			ENSURE(::SHGetFolderPathW(NULL, CSIDL_MYPICTURES, NULL, SHGFP_TYPE_CURRENT, buffer));
-			return std::move(fs::path(buffer));
 		case DIR_USER_QUICK_LAUNCH:
-			return std::move(quick_launch_path(false));
-		case DIR_DEFAULT_USER_QUICK_LAUNCH:
-			return std::move(quick_launch_path(true));
+			return std::move(quick_launch_path());
 		case DIR_TASKBAR_PINS:
 			{
 				fs::path result = get(DIR_USER_QUICK_LAUNCH);
