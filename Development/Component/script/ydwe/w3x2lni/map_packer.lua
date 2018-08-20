45569600274b28c27e5f60547140c2b7764a2d66
最萌小汐 <sumneko@hotmail.com>
2018-08-20 10:44:30 +0800

    命令行转义

--- Development/Component/script/ydwe/w3x2lni/map_packer.lua
+++ Development/Component/script/ydwe/w3x2lni/map_packer.lua
@@ -2,10 +2,19 @@ local process = require 'process'
 local root = fs.ydwe_path()
 local dev = fs.ydwe_devpath()
 
+local function trans_command(cmd)
+    local str = cmd:gsub([[(\\?)$]], function (str)
+        if str == [[\]] then
+            return [[\\]]
+        end
+    end)
+    return '"' .. str .. '"'
+end
+
 return function (mode, map_path, target_path)
     local current_dir = dev / 'plugin' / 'w3x2lni' / 'script'
     local command_line = ([=[
-"%s" -E -e"package.cpath=[[%s]];package.path=[[%s;%s]]" "%s" "%s" "%s" "%s"]=]
+"%s" -E -e"package.cpath=[[%s]];package.path=[[%s;%s]]" "%s" "%s" %s %s]=]
     ):format(
         (root / 'bin' / 'lua.exe'):string(),
         (root / 'bin' / 'modules' / '?.dll'):string(),
@@ -13,8 +22,8 @@ return function (mode, map_path, target_path)
         (current_dir / '?' / 'init.lua'):string(),
         (current_dir / 'gui' / 'mini.lua'):string(),
         mode,
-        map_path:string(),
-        target_path:string()
+        trans_command(map_path:string()),
+        trans_command(target_path:string())
     )
     local p = process()
     p:set_console 'disable'
