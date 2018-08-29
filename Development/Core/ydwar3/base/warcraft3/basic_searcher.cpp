fc9d77232e81df89129445c0aff917781b7a633e
actboy168 <actboy168@gmail.com>
2018-08-29 10:32:46 +0800

    整理代码

--- Development/Core/ydwar3/base/warcraft3/basic_searcher.cpp
+++ Development/Core/ydwar3/base/warcraft3/basic_searcher.cpp
@@ -1,10 +1,48 @@
 #include <base/warcraft3/basic_searcher.h>
-#include <base/warcraft3/detail/memory_search.h>
 extern "C" {
 #include <base/warcraft3/udis86/udis86.h>
 }
 
 namespace base { namespace warcraft3 {
+	namespace detail {
+		static uintptr_t search_str_no_zero(uintptr_t beg, uintptr_t end, const char* str, uintptr_t len)
+		{
+			for (uintptr_t ptr = beg; ptr < end - len; ++ptr)
+			{
+				if (0 == memcmp((const void*)ptr, str, len))
+					return ptr;
+			}
+
+			return 0;
+		}
+
+		static uintptr_t search_str(uintptr_t beg, uintptr_t end, const char* str, uintptr_t len)
+		{
+			for (uintptr_t ptr = beg; ptr < end - len; ++ptr)
+			{
+				if (*(uint8_t*)ptr == 0)
+				{
+					while (*(uint8_t*)ptr == 0) ptr++;
+
+					if (0 == memcmp((const void*)ptr, str, len))
+						return ptr;
+				}
+			}
+
+			return 0;
+		}
+
+		static uintptr_t search_int(uintptr_t beg, uintptr_t end, uint32_t val)
+		{
+			for (uintptr_t ptr = beg; ptr < end - sizeof uint32_t; ++ptr)
+			{
+				if (0 == memcmp((const void*)ptr, &val, sizeof uint32_t))
+					return ptr;
+			}
+
+			return 0;
+		}
+	}
 
 	basic_searcher::basic_searcher(HMODULE hModule)
 		: module_(hModule)
@@ -74,20 +112,6 @@ namespace base { namespace warcraft3 {
 	uintptr_t basic_searcher::search_string(const char* str) const
 	{
 		return search_string(str, strlen(str)+1);
-	}
-
-	uintptr_t basic_searcher::search_string_ptr_part(const char* str, size_t length) const
-	{
-		uintptr_t ptr = search_string_ptr(str, length);
-		if (!ptr)
-		{
-			return 0;
-		}
-		for (; *(unsigned char*)ptr; --ptr)
-		{
-		}
-		ptr++;
-		return ptr;
 	}
 
 	uintptr_t basic_searcher::search_string_part(const char* str) const
