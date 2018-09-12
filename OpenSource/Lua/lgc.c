d4df21d33f9b12de8e83eaf4856229f32c7891a7
actboy168 <actboy168@gmail.com>
2018-09-12 17:56:52 +0800

    lua5.4 patch

--- OpenSource/Lua/lgc.c
+++ OpenSource/Lua/lgc.c
@@ -624,6 +624,10 @@ static int traverseudata (global_State *g, Udata *u) {
   markobjectN(g, u->metatable);  /* mark its metatable */
   for (i = 0; i < u->nuvalue; i++)
     markvalue(g, &u->uv[i].uv);
+  if (g->gckind == KGC_GEN) {
+    linkgclist(u, g->grayagain);  /* keep it in some gray list */
+    black2gray(u);
+  }
   return 1 + u->nuvalue;
 }
 
