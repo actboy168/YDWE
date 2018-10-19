255db83b63b445dbc5ce5085178a5828031aa3a0
actboy168 <actboy168@gmail.com>
2018-10-19 21:38:22 +0800

    1.32.12

--- Development/Core/YDWEVersion/YDWEVersion.h
+++ Development/Core/YDWEVersion/YDWEVersion.h
@@ -3,7 +3,7 @@
 
 #define YDWE_VERSION_MAJOR 1
 #define YDWE_VERSION_MINOR 32
-#define YDWE_VERSION_REVISED 11
+#define YDWE_VERSION_REVISED 12
 #include <YDWEVersionBuild.h>
 
 #endif // YDWE_VERSION_H_INCLUDED
