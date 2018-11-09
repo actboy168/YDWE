7a73c584d020e9ce55153e9ae8b72da8a944b83f
actboy168 <actboy168@gmail.com>
2018-11-09 21:28:37 +0800

    修正一个bug

--- Development/Component/plugin/w3x2lni/plugin/日志路径.lua
+++ Development/Component/plugin/w3x2lni/plugin/日志路径.lua
@@ -6,12 +6,23 @@ mt.info = {
     author = '最萌小汐',
 }
 
+local function ydwePath(support_dev)
+    local ydwe = fs.module_path():parent_path():parent_path()
+    if support_dev then
+        local ydwedev = ydwe:parent_path():parent_path():parent_path()
+        if fs.exists(ydwedev / "build.root") then
+            return ydwedev / "Component"
+        end
+    end
+    return ydwe
+end
+
 function mt:on_convert(w2l)
-    w2l.log_path = fs.ydwe(false) / 'logs' / 'w3x2lni'
+    w2l.log_path = ydwePath(false) / 'logs' / 'w3x2lni'
 end
 
 function mt:on_pack(w2l)
-    w2l.log_path = fs.ydwe(false) / 'logs' / 'w3x2lni'
+    w2l.log_path = ydwePath(false) / 'logs' / 'w3x2lni'
 end
 
 return mt
