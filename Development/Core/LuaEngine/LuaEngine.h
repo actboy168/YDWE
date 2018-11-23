1c57884fce14f4cc5cefec04646bedf947c16f6c
actboy168 <actboy168@gmail.com>
2018-11-24 00:29:07 +0800

    不再需要导出了

--- Development/Core/LuaEngine/LuaEngine.h
+++ Development/Core/LuaEngine/LuaEngine.h
@@ -3,13 +3,6 @@
 #include <base/filesystem.h>
 #include <lua.hpp>
 
-#if defined(LUAENGINE_EXPORTS)
-#	define LUAENGINE_API __declspec(dllexport)
-#else
-#	define LUAENGINE_API __declspec(dllimport)
-#endif
-
-
-LUAENGINE_API lua_State* LuaEngineCreate(const wchar_t* name);
-LUAENGINE_API void       LuaEngineDestory(lua_State* L);
-LUAENGINE_API bool       LuaEngineStart(lua_State* L);
+lua_State* LuaEngineCreate(const wchar_t* name);
+void       LuaEngineDestory(lua_State* L);
+bool       LuaEngineStart(lua_State* L);
